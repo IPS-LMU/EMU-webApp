114a0dc16cdd86af51c5f6336ea12f1c42e68043
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-04-04 15:07:29 +0200

    require added maybe a bad idea

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 HOST = null; // localhost
 PORT = 8001;
-var sys = require("sys"),  
-    http = require("http"),  
-    url = require("url"),  
-    path = require("path"),  
+var sys = require("sys"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path"),
     fs = require("fs");
 var server = http.createServer(function (req, res) {
-  var uri = url.parse(req.url).pathname; 
-  var filename = path.join(process.cwd(), uri);  
-  fs.exists(filename, function (exists) { 
-    if(!exists) {  
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), uri);
+  //console.log(filename);
+  fs.exists(filename, function (exists) {
+    if(!exists) {
       res.writeHead(404, { "Content-Type": "text/plain"});
       res.end("Not Found");
     }
-    fs.readFile(filename, function (err, data) {    
-        if(err) {  
-            res.writeHead(500, {"Content-Type": "text/plain"});  
-            res.end(err + "\n");    
+    fs.readFile(filename, function (err, data) {
+        if(err) {
+          //javascript header check
+          if(filename.indexOf('.js', filename.length - '.js'.length) !== -1){
+            //console.log('yay js');
+            res.writeHead(500, {"Content-Type": "text/javascript"});
+            res.end(err + "\n");
+          }else{
+            //console.log('nay js');
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            res.end(err + "\n");
+          }
             return;
         }
-        res.writeHead(200); 
+        res.writeHead(200);
         res.write(data, "binary");
         res.end();
     });
