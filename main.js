3a593edd4e9151099d95abb76f004d095e7fad40
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-06-26 10:04:24 +0200

    packed app files added

---
+++ main.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ **/
+
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+ chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    bounds: {
+      height: 550,
+      width: 1024,
+      top: 100
+    }
+    //frame: 'none'
+  });
+});
