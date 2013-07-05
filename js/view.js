d0ac5e6cf07094959ee4bda269c9bf6306064504
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-07-05 11:01:24 +0200

    scrollbat width change to be the same as dragbar

--- js/view.js
+++ js/view.js
@@ -16,7 +16,7 @@ EmuLabeller.ViewPort = {
 
         this.curMouseTierID = "";
         this.curMouseBoundary = -1;
-        
+
         this.selectSPixel = 0;
         this.selectEPixel = 0;
     }
