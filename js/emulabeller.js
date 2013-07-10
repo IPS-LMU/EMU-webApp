bbf1f4dedc8a96c5f6372e3db98ae7686642cdab
Georg Raess <georg@raess.name>
2013-07-10 18:34:57 +0200

    space on bottom and top of tiers added

--- js/emulabeller.js
+++ js/emulabeller.js
@@ -339,9 +339,10 @@ var EmuLabeller = {
             if (my.internalMode == my.EDITMODE.DRAGING_BAR) {
                 var diff_Y = event.clientY - my.dragingStartY;
                 var cssDiff = "+=" + diff_Y / 2 + "px";
-                
                 $('#wave').css("height", cssDiff);
                 $('#spectrogram').css("height", cssDiff);
+                var offset = $('#tiers').offset();
+                $('#tiers').offset({ top: offset.top+ diff_Y, left: offset.left})
                 my.dragingStartY = event.clientY;
             }
 
