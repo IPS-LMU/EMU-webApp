da81389109d5f4645bc82fa70b6ad39eaadf239d
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-07-22 17:21:50 +0200

    more grunt stuff…still not really working

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,24 +3,42 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      all: {
+        options: {
+          // separator: ';'
+        },
+        src: ['js/JSONvalidator.js', 'js/socketIOhandler.js', 'js/tierdrawer.js',
+          'js/spectogramDrawer.js', 'js/vars.js', 'js/drawer.js',
+          'js/spectrogram.js', 'js/view.js', 'js/labfileparser.js',
+          'js/ssffdrawer.js', 'js/webaudio.js', 'js/dropBoxHandler.js',
+          'js/layoutHandler.js', 'js/ssffparser.js', 'js/emulabeller.js',
+          'js/main.js', 'js/textgridparser.js', 'js/iohandler.js',
+          'js/oscidrawer.js', 'js/tierHandler.js'
+        ],
+        dest: 'build/emuLVC.js'
+      }
+    },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
+      // options: {
+      //   // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      // },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: {
+          'build/emuLVC.min.js': ['build/emuLVC.js']
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 
 };
