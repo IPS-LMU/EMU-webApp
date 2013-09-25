fb361e3d1d4c27e42d45d7f992574288c79d5c5f
georgraess <j0sh@j0sh.fritz.box>
2013-09-25 12:29:50 +0200

    cleaned mousemovecode

--- app/scripts/directives/trackMouseInTier.js
+++ app/scripts/directives/trackMouseInTier.js
@@ -6,69 +6,42 @@ angular.module('emulvcApp')
     restrict: "A",
     link: function(scope, element){
       var ctx = element[0].getContext('2d');      
-      // variable that decides if something should be drawn on mousemove
-      var drawing = false;     
       // the last coordinates before the current move
       var lastX,currentX;
       var lastY,currentY;
-      element.bind('mousedown', function(event){        
-          currentX = getX(event);
-          currentY = getY(event);
-        // begins new line
-        ctx.beginPath();        
-        drawing = true;
+      // the last event before the current move
+      var lastEvent;
+      
+      element.bind('mousedown', function(event){
+          lastEvent = getEvent(getX(event),event);
       });
       
-      element.bind('dblclick', function(event){ 
-          currentX = getX(event);
-          currentY = getY(event);             
-          alert("dblclick at "+currentX+" "+currentY);
+      element.bind('dblclick', function(event){
+          lastEvent = getEvent(getX(event),event);
+          alert("dblclick at "+lastEvent.label);
       });
       
-      element.bind('mousemove', function(event){
-        if(drawing){
-          // get current mouse position
-          currentX = getX(event);
-          currentY = getY(event);
-
-          draw(lastX, lastY, currentX, currentY);
-
-          // set current coordinates to last one
-          currentX = getX(event);
-          currentY = getY(event);
-        }   
-        var getE = getEvent(getX(event),event);
-        console.log(getE);
+      element.bind('mousemove', function(event){ 
+        lastEvent = getEvent(getX(event),event);
       });
+
       element.bind('mouseup', function(event){
-        // stop drawing
-        drawing = false;
       });      
+
       element.bind('mouseout', function(event){
         $("*").css("cursor", "auto");
       });              
-      // canvas reset
-      function reset(){
-       element[0].width = element[0].width; 
-      }       
+
       function getX(e) {
         return e.offsetX * (e.originalEvent.srcElement.width / e.originalEvent.srcElement.clientWidth);
       }  
       function getY(e) {
         return e.offsetY * (e.originalEvent.srcElement.height / e.originalEvent.srcElement.clientHeight);
       }
-      function draw(lX, lY, cX, cY){
-        // line from
-        ctx.moveTo(lX,lY);
-        // to
-        ctx.lineTo(cX,cY);
-        // color
-        ctx.strokeStyle = "#f00";
-        // draw it
-        ctx.stroke();
-      }
       function getEvent(x,event) {
-        var pcmpp = 128085/event.originalEvent.srcElement.width;
+        var lastStart = scope.tierDetails.events[scope.tierDetails.events.length-1].startSample;
+        var length = lastStart + scope.tierDetails.events[scope.tierDetails.events.length-1].sampleDur;
+        var pcmpp = length/event.originalEvent.srcElement.width;
         var pcm = x * pcmpp; 
         var evtr = null;
 		angular.forEach(scope.tierDetails.events, function(evt) {
