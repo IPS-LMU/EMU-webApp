b995f41174402085f4cdb694940287a93c2cef65
georgraess <j0sh@m159.lmu.vpn.lrz.de>
2013-10-14 11:26:50 +0200

    tier resize error fixes

--- app/scripts/directives/resize.js
+++ app/scripts/directives/resize.js
@@ -8,22 +8,27 @@ angular.module('emulvcApp')
 
       var elem = element.parent().parent();    
       var canvas = elem.find("canvas");
-      var del = $("div.delete")[0];
-      var sav = $("div.save")[0];
+      var del = element.parent().children()[0];
+      var sav = element.parent().children()[2];
       var b_delete = elem.find(del);
       var b_save = elem.find(sav);
       var open = true;
+      var orignalSize;
 
       element.bind('click', function(event){
         if(open) {
           open = false;
-          canvas[0].height /= 3;
+          //console.log(canvas[0].style.height);
+          orignalSize = canvas[0].height;
+          canvas[0].height = orignalSize/3;
           b_delete.hide();
           b_save.hide();
+          //console.log(element.parent().children());
         }
         else {
           open = true;
-          canvas[0].height *= 3;        
+          //console.log(canvas[0].style.height);
+          canvas[0].height = orignalSize;        
           b_delete.show();
           b_save.show();
         }
