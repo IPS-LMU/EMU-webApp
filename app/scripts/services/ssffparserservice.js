9b8e37a72280352f2c4552611ea49d1895eebe61
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2013-10-14 18:26:42 +0200

    more parser improvements

--- app/scripts/services/ssffparserservice.js
+++ app/scripts/services/ssffparserservice.js
@@ -17,11 +17,8 @@ angular.module('emulvcApp')
 		};
 
 		sServObj.ssffData.sampleRate = -1;
-		sServObj.ssffData.origFreq = -1;
 		sServObj.ssffData.startTime = -1;
-		sServObj.ssffData.startRecord = -1;
-		sServObj.ssffData.endRecord = -1;
-		sServObj.ssffData.ssffColumnTypes = {};
+		sServObj.ssffData.origFreq = -1;
 		sServObj.ssffData.Columns = [];
 
 
@@ -43,15 +40,17 @@ angular.module('emulvcApp')
 			}
 			// check if Record_Freq+Start_Time is there
 			if (newLsep[2].split(/[ ,]+/)[0] != "Record_Freq " || newLsep[3].split(/[ ,]+/)[0] != "Start_Time ") {
-				this.ssffData.sampleRate = newLsep[2].split(/[ ,]+/)[1];
-				this.ssffData.startTime = newLsep[3].split(/[ ,]+/)[1];
+				this.ssffData.sampleRate = parseFloat(newLsep[2].split(/[ ,]+/)[1].replace(/(\r\n|\n|\r)/gm,""));
+				this.ssffData.startTime = parseFloat(newLsep[3].split(/[ ,]+/)[1].replace(/(\r\n|\n|\r)/gm,""));
 			} else {
 				alert('no ssff file... or missing fields ');
 			}
 
 			for (var i = 4; i < newLsep.length; i++) {
+				if (newLsep[i].split(/[ ,]+/)[0] == "Original_Freq") {
+					this.ssffData.origFreq = parseFloat(newLsep[i].split(/[ ,]+/)[2].replace(/(\r\n|\n|\r)/gm,""));
+				}
 				if (newLsep[i] == this.sepString) {
-					//console.log(newLsep[i]);
 					break;
 				}
 				var lSpl = newLsep[i].split(/[ ]+/);
@@ -59,11 +58,10 @@ angular.module('emulvcApp')
 				if (lSpl[0] == "Column") {
 					this.ssffData.Columns.push({
 						"name": lSpl[1],
-						"type": lSpl[2],
+						"ssffdatatype": lSpl[2],
 						"length": lSpl[3],
 						"values": []
 					});
-					this.ssffData.ssffColumnTypes[lSpl[1]] = lSpl[2];
 				}
 
 			}
@@ -77,30 +75,23 @@ angular.module('emulvcApp')
 
 				for (i = 0; i < this.ssffData.Columns.length; i++) {
 					//console.log(this.ssffData.Columns[i].length);
-					if (this.ssffData.Columns[i].type == "DOUBLE") {
-
+					if (this.ssffData.Columns[i].ssffdatatype == "DOUBLE") {
 						curLen = 8 * this.ssffData.Columns[i].length;
 						curBuffer = buf.subarray(curBinIdx, curLen);
-						
 						curBufferView = new Float64Array(curBuffer);
-
 						this.ssffData.Columns[i].values.push(curBufferView);
-
 						curBinIdx += curLen;
 
-					} else if (this.ssffData.Columns[i].type == "FLOAT") {
-
+					} else if (this.ssffData.Columns[i].ssffdatatype == "FLOAT") {
 						curLen = 4 * this.ssffData.Columns[i].length;
 						curBuffer = buf.subarray(curBinIdx, curLen);
-
 						curBufferView = new Float32Array(curBuffer);
 						this.ssffData.Columns[i].values.push(curBufferView);
-
 						curBinIdx += curLen;
 
 					} else {
 						//console.log("fasdfsd");
-						alert("not supported... only doubles for now");
+						alert("not supported... only doubles and floats for now");
 					}
 
 				} //for
