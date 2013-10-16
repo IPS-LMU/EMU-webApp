9ee16ba21e2add25ba3fb16d0bf5a37b5e2ff830
georgraess <j0sh@j0sh.lan>
2013-10-16 19:45:23 +0200

    some css changes

--- app/scripts/directives/draggableResize.js
+++ app/scripts/directives/draggableResize.js
@@ -6,15 +6,20 @@ angular.module("emulvcApp")
       restrict: "A",
       link: function(scope, element) {
       
-        var start = 0;
-        var now = 0;
-        var diff = 0;
+        var last = 0;
             
         element.draggable({ 
             axis: "y",
             cursor: "move", 
             cursorAt: { bottom: 8 },
-            handle: "div" 
+            handle: "div",
+            containment: "parent",
+		
+			drag: function(e, ui) {
+
+			    console.log(ui.offset.top);
+			    
+			},	            
         });
       
       
