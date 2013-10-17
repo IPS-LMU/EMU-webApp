5f7191a52413a1a6e8a6d6ea1990e4b8f0daec12
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2013-10-17 13:31:48 +0200

    reformat

--- app/scripts/directives/correctiontool.js
+++ app/scripts/directives/correctiontool.js
@@ -13,7 +13,7 @@ angular.module('emulvcApp')
 						scope.$apply(
 							scope.ssffData[0].Columns[0].values.forEach(function(arr, arrIDX) {
 								if (scope.vs.curCorrectionToolNr != -1) {
-									arr[scope.vs.curCorrectionToolNr - 1] = 8000-scope.dhs.getY(event) / event.originalEvent.srcElement.height * 8000; // SIC hardcoded
+									arr[scope.vs.curCorrectionToolNr - 1] = 8000 - scope.dhs.getY(event) / event.originalEvent.srcElement.height * 8000; // SIC hardcoded
 								}
 							})
 						);
