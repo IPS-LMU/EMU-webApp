b9dc546c9f70b3ee6812e861167c0c9a9047a735
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2013-10-21 14:12:26 +0200

    removed commented getPeaks func.

--- app/scripts/directives/osci.js
+++ app/scripts/directives/osci.js
@@ -34,68 +34,6 @@ angular.module('emulvcApp')
 				
 
 
-				// /**
-				//  * get current peaks to be drawn
-				//  * if drawing over sample exact -> samples
-				//  * if multiple samples per pixel -> calculate envelope points
-				//  */
-
-				// function getPeaks(viewState, canvas, buffer) {
-				// 	var k = (viewState.curViewPort.eS - viewState.curViewPort.sS) / canvas.width; //this.osciCanvas.width; // PCM Samples per new pixel
-
-				// 	var peaks = [];
-				// 	var minPeak = Infinity;
-				// 	var maxPeak = -Infinity;
-
-				// 	var chan = buffer.getChannelData(0);
-				// 	var relData;
-
-				// 	if (k <= 1) {
-				// 		// check if view at start            
-				// 		if (viewState.curViewPort.sS === 0) {
-				// 			relData = chan.subarray(viewState.curViewPort.sS, viewState.curViewPort.eS + 2); // +2 to compensate for length
-				// 		} else {
-				// 			relData = chan.subarray(viewState.curViewPort.sS - 1, viewState.curViewPort.eS + 2); // +2 to compensate for length
-				// 		}
-				// 		minPeak = Math.min.apply(Math, relData);
-				// 		maxPeak = Math.max.apply(Math, relData);
-				// 		peaks = Array.prototype.slice.call(relData);
-
-				// 	} else {
-				// 		relData = chan.subarray(viewState.curViewPort.sS, viewState.curViewPort.eS);
-
-				// 		for (var i = 0; i < canvas.width; i++) {
-				// 			var sum = 0;
-				// 			for (var c = 0; c < buffer.numberOfChannels; c++) {
-
-				// 				var vals = relData.subarray(i * k, (i + 1) * k);
-				// 				var peak = -Infinity;
-
-				// 				var av = 0;
-				// 				for (var p = 0, l = vals.length; p < l; p++) {
-				// 					if (vals[p] > peak) {
-				// 						peak = vals[p];
-				// 					}
-				// 					av += vals[p];
-				// 				}
-				// 				sum += av / vals.length;
-				// 			}
-
-				// 			peaks[i] = sum;
-				// 			if (sum > maxPeak) {
-				// 				maxPeak = sum;
-				// 			}
-				// 		}
-				// 	} //else
-				// 	return {
-				// 		"peaks": peaks,
-				// 		"minPeak": minPeak,
-				// 		"maxPeak": maxPeak,
-				// 		"samplePerPx": k
-				// 	};
-				// }
-
-
 				/**
 				 * @param cps color provider service
 				 */
