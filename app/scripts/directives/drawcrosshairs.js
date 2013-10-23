1247ec21348b600d704ccf49dc9b0ab138a2012b
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2013-10-23 18:09:34 +0200

    crosshairs now working on markup canvas

--- app/scripts/directives/drawcrosshairs.js
+++ app/scripts/directives/drawcrosshairs.js
@@ -4,22 +4,29 @@ angular.module('emulvcApp')
 	.directive('drawcrosshairs', function() {
 		return {
 			restrict: 'A',
-			link: function postLink(scope, element, attrs) {
-
-				// element.text('this is the drawcrosshairs directive');
+			link: function postLink(scope, element) {
+				var canvas = element[0];
 
 				// on mouse move
 				element.bind('mousemove', function(event) {
-					console.log()
-					// switch (event.which) {
-						// case 1:
-							//console.log('Left mouse button pressed');
-							// thisPCM = getX(event) * scope.vs.getPCMpp(event) + scope.vs.curViewPort.sS;
-							// scope.vs.select(startPCM, thisPCM);
-							// scope.$apply();
-							// break;
-					// }
+					drawCrossHairs(scope.vs, canvas, scope.config, scope.dhs, event);
 				});
+
+				function drawCrossHairs(viewState, canvas, config, dhs, mouseEvt) {
+					var ctx = canvas.getContext('2d');
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					ctx.strokeStyle = config.vals.colors.crossHairsColor;
+					ctx.fillStyle = config.vals.colors.crossHairsColor;
+
+					// draw lines
+					ctx.beginPath();
+					ctx.moveTo(0, dhs.getY(mouseEvt));
+					ctx.lineTo(canvas.width, dhs.getY(mouseEvt));
+					ctx.moveTo(dhs.getX(mouseEvt), 0);
+					ctx.lineTo(dhs.getX(mouseEvt), canvas.height);
+					ctx.stroke();
+				}
+
 			}
 		};
 	});
