f5f2f649a96a04296791895062abae66bf79feb7
georgraess <j0sh@j0sh.lan>
2013-10-23 15:53:26 +0200

    preview corrections

--- app/scripts/directives/preview.js
+++ app/scripts/directives/preview.js
@@ -9,18 +9,23 @@ angular.module('emulvcApp')
 			link: function postLink(scope, element, attrs) {
 				// select the needed DOM elements from the template
 				var canvas = element.find("canvas")[0];
-				var myid = element[0].id;			
+				var myid = element[0].id;		
+				var initialized = false;	
+				var cacheImage = new Image();
 				
 				scope.$watch('vs.curViewPort', function(newValue, oldValue) {
 					if (!$.isEmptyObject(scope.shs.currentBuffer)) {
-						// check for changed zoom
-						if(oldValue.sS != newValue.sS || oldValue.sE != newValue.sE || newValue.selectS == -1){ // SIC -1 check not that clean...
+					    if(!initialized) {
 							var allPeakVals = scope.dhs.calculatePeaks(scope.vs, canvas, scope.shs.currentBuffer.getChannelData(0));
 							scope.dhs.osciPeaks = allPeakVals;
-							
+							scope.dhs.freshRedrawDrawOsciOnCanvas(scope.vs, canvas, scope.dhs.osciPeaks, scope.shs.currentBuffer, scope.config);
+							cacheImage.src = canvas.toDataURL("image/png");
+							initialized = true;
 						}
-						scope.dhs.freshRedrawDrawOsciOnCanvas(scope.vs, canvas, scope.dhs.osciPeaks, scope.shs.currentBuffer, scope.config);
-						drawVpOsciMarkup(scope.vs, canvas, scope.config);						
+						else {
+						    drawVpOsciMarkup(scope.vs, canvas, scope.config, cacheImage);
+						}
+						
 					}
 				}, true);			
 
@@ -30,36 +35,31 @@ angular.module('emulvcApp')
 				 * the viewport
 				 */
 
-				function drawVpOsciMarkup(viewState, canvas, config) {
-
+				function drawVpOsciMarkup(vs, canvas, config, cacheImage) {
 					var ctx = canvas.getContext("2d");
-
-					ctx.strokeStyle = config.vals.colors.labelColor;
-					ctx.fillStyle = config.vals.colors.labelColor;
-					ctx.font = (config.vals.colors.fontPxSize + "px" + " " + config.vals.colors.fontType);
-
-					// lines to corners
+					var image = new Image();
+					var posS = (canvas.width/vs.curViewPort.bufferLength) * vs.curViewPort.sS;
+					var posE = (canvas.width/vs.curViewPort.bufferLength) * vs.curViewPort.eS;
+					var sDist = vs.getSampleDist(canvas.width);
+					console.log(posS,posE);
+                    image.onload = function() {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        ctx.drawImage(image, 0, 0);
+					
+					ctx.fillStyle = config.vals.colors.selectedAreaColor;
+					ctx.fillRect(posS, 0, posE - posS, canvas.height);
+					ctx.strokeStyle = config.vals.colors.selectedBoundaryColor;
 					ctx.beginPath();
-					ctx.moveTo(0, 0);
-					ctx.lineTo(5, 5);
-					ctx.moveTo(canvas.width, 0);
-					ctx.lineTo(canvas.width - 5, 5);
+					ctx.moveTo(posS, 0);
+					ctx.lineTo(posS, canvas.height);
+					ctx.moveTo(posE, 0);
+					ctx.lineTo(posE, canvas.height);
 					ctx.closePath();
 					ctx.stroke();
 
-					var sTime;
-					var eTime;
-					if (viewState.curViewPort) {
-						//draw time and sample nr
-						sTime = viewState.round(viewState.curViewPort.sS / 44100, 6); //SIC hardcoded sample rate
-						eTime = viewState.round(viewState.curViewPort.eS / 44100, 6); //SIC hardcoded sample rate
-						ctx.fillText(viewState.curViewPort.sS, 5, config.vals.colors.fontPxSize);
-						ctx.fillText(sTime, 5, config.vals.colors.fontPxSize * 2);
-						var metrics = ctx.measureText(sTime);
-						ctx.fillText(viewState.curViewPort.eS, canvas.width - ctx.measureText(viewState.curViewPort.eS).width - 5, config.vals.colors.fontPxSize);
-						ctx.fillText(eTime, canvas.width - metrics.width - 5, config.vals.colors.fontPxSize * 2);
-					}
-				}
+                    };
+                    image.src = cacheImage.src;
+				}			
 			}
 		};
 	});
