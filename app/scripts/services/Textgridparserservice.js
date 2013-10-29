ae16e26453ce3f418db97b0b4aab40aaf9e7313e
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2013-10-29 15:15:15 +0100

    jshint cleanup

--- app/scripts/services/Textgridparserservice.js
+++ app/scripts/services/Textgridparserservice.js
@@ -23,7 +23,6 @@ angular.module('emulvcApp')
 			string = string.replace(/[ \t]+/g, '');
 			var lines = string.split('\n');
 
-			var tiers = [];
 			var tT, tN, eT, lab;
 			var inHeader = true;
 
@@ -34,18 +33,20 @@ angular.module('emulvcApp')
 				tiers: []
 			};
 
-			if (lines[0].replace(/\s+/g, '') == this.l1.replace(/\s+/g, '') && lines[1].replace(/\s+/g, '') == this.l2.replace(/\s+/g, '')) {
+			if (lines[0].replace(/\s+/g, '') === this.l1.replace(/\s+/g, '') && lines[1].replace(/\s+/g, '') === this.l2.replace(/\s+/g, '')) {
 				for (var i = 2; i < lines.length; i++) {
 					var cL = lines[i];
-					if (cL == 'item[]:') {
+					if (cL === 'item[]:') {
 						inHeader = false;
 						continue;
 					}
-					if (inHeader) continue;
+					if (inHeader) {
+						continue;
+					}
 
 					if (cL.indexOf('item[') === 0) {
 						// get tier type
-						if (lines[i + 1].split(/=/)[1] == '\"IntervalTier\"') {
+						if (lines[i + 1].split(/=/)[1] === '\"IntervalTier\"') {
 							tT = 'seg';
 						} else {
 							tT = 'point';
@@ -60,10 +61,8 @@ angular.module('emulvcApp')
 							events: []
 						});
 					}
-					if (labelJSO.tiers.length > 0 && labelJSO.tiers[labelJSO.tiers.length - 1].type == 'seg' && (cL.indexOf('intervals') === 0) && (cL.indexOf('intervals:') !== 0)) {
+					if (labelJSO.tiers.length > 0 && labelJSO.tiers[labelJSO.tiers.length - 1].type === 'seg' && (cL.indexOf('intervals') === 0) && (cL.indexOf('intervals:') !== 0)) {
 						// parse seg tiers event
-						console.log("##############################")
-						console.log(this.shs.wavJSO.SampleRate)
 						var eSt = Math.ceil(lines[i + 1].split(/=/)[1] * this.shs.wavJSO.SampleRate);
 						var eEt = Math.floor(lines[i + 2].split(/=/)[1] * this.shs.wavJSO.SampleRate);
 						lab = lines[i + 3].split(/=/)[1].replace(/"/g, '');
@@ -78,7 +77,7 @@ angular.module('emulvcApp')
 							startSample: eSt - 1, // correct so starts at 0
 							sampleDur: eEt - eSt
 						});
-					} else if (labelJSO.tiers.length > 0 && labelJSO.tiers[labelJSO.tiers.length - 1].type == 'point' && cL.indexOf('points') === 0 && cL.indexOf('points:') !== 0) {
+					} else if (labelJSO.tiers.length > 0 && labelJSO.tiers[labelJSO.tiers.length - 1].type === 'point' && cL.indexOf('points') === 0 && cL.indexOf('points:') !== 0) {
 						// parse point tier event
 						eT = lines[i + 1].split(/=/)[1] * this.shs.wavJSO.SampleRate;
 						lab = lines[i + 2].split(/=/)[1].replace(/"/g, '');
@@ -90,8 +89,8 @@ angular.module('emulvcApp')
 					}
 
 				}
-				console.log(JSON.stringify(labelJSO, undefined, 2));
-				// this.testForGapsInLabelJSO(labelJSO);
+				// console.log(JSON.stringify(labelJSO, undefined, 2));
+				this.testForGapsInLabelJSO(labelJSO);
 				return labelJSO;
 
 			} else {
@@ -100,6 +99,25 @@ angular.module('emulvcApp')
 
 		};
 
+
+
+		/**
+		 * test to see if all the segments in seg tiers
+		 * are "snapped" -> startSample+dur+1 = startSample of next Segment
+		 */
+		sServObj.testForGapsInLabelJSO = function(labelJSO) {
+			var counter = 0;
+			for (var i = 0; i < labelJSO.tiers.length; i++) {
+				if (labelJSO.tiers[i].type === 'seg') {
+					for (var j = 0; j < labelJSO.tiers[i].events.length - 1; j++) {
+						if (labelJSO.tiers[i].events[j].startSample + labelJSO.tiers[i].events[j].sampleDur + 1 !== labelJSO.tiers[i].events[j + 1].startSample) {
+							counter = counter + 1;
+						}
+					}
+				}
+			}
+			console.log('TextGridParser had: ', counter, 'alignment issues found in parsed TextGrid');
+		};
 
 		return sServObj;
 	});
