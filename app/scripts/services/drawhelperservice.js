8b8bd4e5daf60a044f5b41dac27468ade9305465
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-11-05 15:59:07 +0100

    fixed dashed line... now created black fantom line though...

--- app/scripts/services/drawhelperservice.js
+++ app/scripts/services/drawhelperservice.js
@@ -90,8 +90,8 @@ angular.module('emulvcApp')
 
 
 		/**
-		* @param cps color provider service
-		*/
+		 * @param cps color provider service
+		 */
 
 		sServObj.freshRedrawDrawOsciOnCanvas = function(viewState, canvas, allPeakVals, buffer, config) {
 			var ctx = canvas.getContext("2d");
@@ -107,126 +107,134 @@ angular.module('emulvcApp')
 					}
 				});
 
-					} else if (allPeakVals.samplePerPx < 1) {
-						// console.log("at 0 over sample exact")
-						var hDbS = (1 / allPeakVals.samplePerPx) / 2; // half distance between samples
-						var sNr = viewState.curViewPort.sS;
-						// over sample exact
-						ctx.strokeStyle = config.vals.colors.osciColor;
-						ctx.fillStyle = config.vals.colors.osciColor;
-						// ctx.beginPath();
-						var i;
-						if (viewState.curViewPort.sS === 0) {
-							ctx.moveTo(hDbS, (allPeakVals.peaks[0] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height);
-							for (i = 0; i < allPeakVals.peaks.length; i++) {
-								ctx.lineTo(i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height);
-							}
-							ctx.stroke();
-							// draw sample dots
-							for (i = 0; i < allPeakVals.peaks.length; i++) {
-								ctx.beginPath();
-								ctx.arc(i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 3, 4, 0, 2 * Math.PI, false);
-								ctx.stroke();
-								ctx.fill();
-								if (false) { // SIC !!!
-									ctx.strokeText(sNr, i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 10);
-									sNr = sNr + 1;
-								}
-							}
-						} else {
-							//draw lines
-							ctx.moveTo(-hDbS, canvas.height - ((allPeakVals.peaks[0] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height));
-							for (i = 1; i < allPeakVals.peaks.length; i++) {
-								ctx.lineTo(i / allPeakVals.samplePerPx - hDbS, canvas.height - ((allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height + 3));
-							}
-							ctx.stroke();
-							// draw sample dots
-							for (i = 1; i < allPeakVals.peaks.length; i++) {
-								ctx.beginPath();
-								ctx.arc(i / allPeakVals.samplePerPx - hDbS, canvas.height - ((allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height) - 3, 4, 0, 2 * Math.PI, false);
-								ctx.stroke();
-								ctx.fill();
-								if (false) { // SIC !!! 
-									ctx.fillText(sNr, i / allPeakVals.samplePerPx - hDbS, canvas.height - (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 10);
-									sNr = sNr + 1;
-								}
-							}
-
-						}
+			} else if (allPeakVals.samplePerPx < 1) {
+				// console.log("at 0 over sample exact")
+				var hDbS = (1 / allPeakVals.samplePerPx) / 2; // half distance between samples
+				var sNr = viewState.curViewPort.sS;
+				// over sample exact
+				ctx.strokeStyle = config.vals.colors.osciColor;
+				ctx.fillStyle = config.vals.colors.osciColor;
+				// ctx.beginPath();
+				var i;
+				if (viewState.curViewPort.sS === 0) {
+					ctx.moveTo(hDbS, (allPeakVals.peaks[0] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height);
+					for (i = 0; i < allPeakVals.peaks.length; i++) {
+						ctx.lineTo(i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height);
 					}
-					if (true) { // SIC !!!
-						// draw zero line
-						ctx.strokeStyle = config.vals.colors.zeroLineColor;
-						ctx.fillStyle = config.vals.colors.wwzeroLineColor;
-						// see if Chrome ->dashed line
-						if (navigator.vendor == "Google Inc.") {
-							ctx.setLineDash([5]);
-						}
-						if (allPeakVals.samplePerPx >= 1) {
-							ctx.strokeRect(0, canvas.height / 2, canvas.width, 2);
-							ctx.fillText("0", 5, canvas.height / 2 - 5, canvas.width);
-						} else {
-							ctx.strokeRect(0, canvas.height - ((0 - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height), canvas.width, 1);
-							ctx.fillText("0", 5, canvas.height - ((0 - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height) - 5, canvas.width);
-						}
-						// see if Chrome ->dashed line
-						if (navigator.vendor == "Google Inc.") {
-							ctx.setLineDash([0]);
-						}
-					}
-				}
-
-				/**
-				 * drawing method to draw single line between two
-				 * envelope points. Is used by drawOsciOnCanvas if
-				 * envelope drawing is done
-				 * @param index
-				 * @param value
-				 * @param max
-				 * @param prevPeak
-				 * @param canvas
-				 */
-
-				function drawFrame(viewState, index, value, max, prevPeak, canvas, config) {
-
-					var ctx = canvas.getContext("2d");
-
-					//calculate sample of cur cursor position
-
-					//calc cursor pos
-					var all = viewState.curViewPort.eS - viewState.curViewPort.sS;
-					var fracC = viewState.curCursorPosInPercent * viewState.bufferLength - viewState.curViewPort.sS;
-					var procC = fracC / all;
-					var posC = canvas.width * procC;
-
-					//cur
-					var w = 1;
-					var h = Math.round(value * (canvas.height / max)); //rel to max
-					var x = index * w;
-					var y = Math.round((canvas.height - h) / 2);
-
-					//prev
-					var prevW = 1;
-					var prevH = Math.round(prevPeak * (canvas.height / max));
-					var prevX = (index - 1) * w;
-					var prevY = Math.round((canvas.height - prevH) / 2);
-
-
-					if (posC >= x) {
-						ctx.fillStyle = config.vals.colors.playProgressColor;
-						ctx.strokeStyle = config.vals.colors.playProgressColor;
-					} else {
-						ctx.fillStyle = config.vals.colors.osciColor;
-						ctx.strokeStyle = config.vals.colors.osciColor;
-					}
-
-					ctx.beginPath();
-					ctx.moveTo(prevX, prevY);
-					ctx.lineTo(x, y);
 					ctx.stroke();
+					// draw sample dots
+					for (i = 0; i < allPeakVals.peaks.length; i++) {
+						ctx.beginPath();
+						ctx.arc(i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 3, 4, 0, 2 * Math.PI, false);
+						ctx.stroke();
+						ctx.fill();
+						if (false) { // SIC !!!
+							ctx.strokeText(sNr, i / allPeakVals.samplePerPx + hDbS, (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 10);
+							sNr = sNr + 1;
+						}
+					}
+				} else {
+					//draw lines
+					ctx.moveTo(-hDbS, canvas.height - ((allPeakVals.peaks[0] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height));
+					for (i = 1; i < allPeakVals.peaks.length; i++) {
+						ctx.lineTo(i / allPeakVals.samplePerPx - hDbS, canvas.height - ((allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height + 3));
+					}
+					ctx.stroke();
+					// draw sample dots
+					for (i = 1; i < allPeakVals.peaks.length; i++) {
+						ctx.beginPath();
+						ctx.arc(i / allPeakVals.samplePerPx - hDbS, canvas.height - ((allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height) - 3, 4, 0, 2 * Math.PI, false);
+						ctx.stroke();
+						ctx.fill();
+						if (false) { // SIC !!! 
+							ctx.fillText(sNr, i / allPeakVals.samplePerPx - hDbS, canvas.height - (allPeakVals.peaks[i] - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height - 10);
+							sNr = sNr + 1;
+						}
+					}
 
 				}
-				
+			}
+			if (true) { // SIC !!!
+				// draw zero line
+				ctx.strokeStyle = config.vals.colors.zeroLineColor;
+				ctx.fillStyle = config.vals.colors.zeroLineColor;
+				// see if Chrome ->dashed line
+				if (navigator.vendor == "Google Inc.") {
+					ctx.setLineDash([2]);
+				}
+				if (allPeakVals.samplePerPx >= 1) {
+					ctx.beginPath();
+					ctx.moveTo(0, canvas.height / 2);
+					ctx.lineTo(canvas.width, canvas.height / 2);
+					ctx.stroke();
+					ctx.fillText("0", 5, canvas.height / 2 - 5, canvas.width);
+				} else {
+					var zeroLineY = canvas.height - ((0 - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height);
+					ctx.beginPath();
+					ctx.moveTo(0, zeroLineY);
+					ctx.lineTo(canvas.width, zeroLineY);
+					ctx.stroke();
+					ctx.fill();
+					ctx.fillText("0", 5, canvas.height - ((0 - allPeakVals.minPeak) / (allPeakVals.maxPeak - allPeakVals.minPeak) * canvas.height) - 5, canvas.width);
+				}
+				// see if Chrome ->dashed line
+				if (navigator.vendor == "Google Inc.") {
+					ctx.setLineDash([0]);
+				}
+			}
+		}
+
+		/**
+		 * drawing method to draw single line between two
+		 * envelope points. Is used by drawOsciOnCanvas if
+		 * envelope drawing is done
+		 * @param index
+		 * @param value
+		 * @param max
+		 * @param prevPeak
+		 * @param canvas
+		 */
+
+		function drawFrame(viewState, index, value, max, prevPeak, canvas, config) {
+
+			var ctx = canvas.getContext("2d");
+
+			//calculate sample of cur cursor position
+
+			//calc cursor pos
+			var all = viewState.curViewPort.eS - viewState.curViewPort.sS;
+			var fracC = viewState.curCursorPosInPercent * viewState.bufferLength - viewState.curViewPort.sS;
+			var procC = fracC / all;
+			var posC = canvas.width * procC;
+
+			//cur
+			var w = 1;
+			var h = Math.round(value * (canvas.height / max)); //rel to max
+			var x = index * w;
+			var y = Math.round((canvas.height - h) / 2);
+
+			//prev
+			var prevW = 1;
+			var prevH = Math.round(prevPeak * (canvas.height / max));
+			var prevX = (index - 1) * w;
+			var prevY = Math.round((canvas.height - prevH) / 2);
+
+
+			if (posC >= x) {
+				ctx.fillStyle = config.vals.colors.playProgressColor;
+				ctx.strokeStyle = config.vals.colors.playProgressColor;
+			} else {
+				ctx.fillStyle = config.vals.colors.osciColor;
+				ctx.strokeStyle = config.vals.colors.osciColor;
+			}
+
+			ctx.beginPath();
+			ctx.moveTo(prevX, prevY);
+			ctx.lineTo(x, y);
+			ctx.stroke();
+
+		}
+
 
 		return sServObj;
 	});
