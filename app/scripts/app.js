7c585ef984ddb8dd2913fdd9da2c061167634988
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-12-05 14:36:49 +0100

    controller around main.html not in routeprovider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,8 +5,7 @@ angular.module('emulvcApp', ['ui', 'ui.bootstrap', 'ngRoute'])
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/main.html'
       })
       .otherwise({
         redirectTo: '/'
