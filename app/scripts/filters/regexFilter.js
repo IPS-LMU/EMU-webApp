ded72a766fdbd65baf6a41251f9708985cf2281d
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-12-12 15:50:42 +0100

    full regex matching now possible

--- app/scripts/filters/regexFilter.js
+++ app/scripts/filters/regexFilter.js
@@ -2,12 +2,13 @@
 
 angular.module('emulvcApp')
 	.filter('regex', function () {
-		return function (input, field, regex) {
-			var patt = new RegExp(regex);
+		return function (input, regex) {
+			var patt = new RegExp(regex.toLowerCase());
 			var out = [];
 			for (var i = 0; i < input.length; i++) {
-				if (patt.test(input[i][field]))
+				if (patt.test(input[i].name.toLowerCase())) {
 					out.push(input[i]);
+				}
 			}
 			return out;
 		};
