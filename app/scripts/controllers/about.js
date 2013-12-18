a062819d99a0e603d3e99aa8a665e792e4d55256
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2013-12-18 11:06:46 +0100

    stated fixing about dial

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -4,6 +4,10 @@ angular.module('emulvcApp')
 	.controller('AboutCtrl', function ($scope, ConfigProviderService, dialogService) {
 		$scope.cps = ConfigProviderService;
 
+		$scope.getStrRep = function (code) {
+			return String.fromCharCode(code);
+		};
+
 		$scope.cancel = function () {
 			dialogService.close();
 		};
