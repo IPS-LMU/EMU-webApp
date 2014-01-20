e4f6c9e01ad9ce5b052eb8572b973703c34e7ab3
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2014-01-20 16:28:30 +0100

    spect settings e2e tests

---
+++ test/e2e/spectroSettings.spec.js
@@ -0,0 +1,122 @@
+// tests for Utterence filter 
+describe('spectrogram settings', function () {
+
+	var ptor;
+
+	browser.get('http://127.0.0.1:9000/');
+	// load demo
+	element(by.id('openDemoDBbtn')).click();
+
+	// beforeEach it
+	beforeEach(function () {
+		ptor = protractor.getInstance();
+	});
+
+	// afterEach it
+	afterEach(function () {
+		// reset to default values
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.rangeFrom')).clear()
+		element(by.input('modalVals.rangeFrom')).sendKeys('0');
+		element(by.input('modalVals.rangeTo')).clear()
+		element(by.input('modalVals.rangeTo')).sendKeys('5000');
+		element(by.input('modalVals.windowLength')).clear()
+		element(by.input('modalVals.windowLength')).sendKeys('256');
+		element(by.input('modalVals.dynamicRange')).clear()
+		element(by.input('modalVals.dynamicRange')).sendKeys('70');
+		element(by.id('dialogSaveButton')).click();
+	});
+
+
+
+	it('should be able to set view range of spectrogram settings top limit to 8000 Hz', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.rangeTo')).clear()
+		element(by.input('modalVals.rangeTo')).sendKeys('8000');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText'); // see if filtertext input filed exists SIC... but maybe not so bad
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	it('should be able to set view range of spectrogram settings bottom limit to 2000 Hz', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.rangeFrom')).clear()
+		element(by.input('modalVals.rangeFrom')).sendKeys('1000');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	it('should be able to set window length to 512 Samples', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.windowLength')).clear()
+		element(by.input('modalVals.windowLength')).sendKeys('512');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	it('should be able to set window length to 128 Samples', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.windowLength')).clear()
+		element(by.input('modalVals.windowLength')).sendKeys('128');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	it('should be able to set dynamic range to 50', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.dynamicRange')).clear()
+		element(by.input('modalVals.dynamicRange')).sendKeys('50');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	it('should be able to set dynamic range to 90', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.input('modalVals.dynamicRange')).clear()
+		element(by.input('modalVals.dynamicRange')).sendKeys('90');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.input('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+
+	// it('should be able window function to BARLETT', function () {
+	// 	element(by.id('spectSettingsBtn')).click();
+
+	// 	// element(by.input('selWindowInfo.name')).click();
+	// 	// element(by.css('option[value=0]')).click();
+
+	// 	// element(by.id('dialogSaveButton')).click();
+	// 	// var ele = by.input('filterText');
+	// 	// expect(ptor.isElementPresent(ele)).toBe(true);
+	// });
+
+	function selectOption(selector, item) {
+		var selectList, desiredOption;
+
+		selectList = this.findElement(selector);
+		selectList.click();
+
+		selectList.findElements(protractor.By.tagName('option'))
+			.then(function findMatchingOption(options) {
+				options.some(function (option) {
+					option.getText().then(function doesOptionMatch(text) {
+						if (item === text) {
+							desiredOption = option;
+							return true;
+						}
+					});
+				});
+			})
+			.then(function clickOption() {
+				if (desiredOption) {
+					desiredOption.click();
+				}
+			});
+	}
+
+
+});
--- test/e2e/uttFilter.spec.js
+++ test/e2e/uttFilter.spec.js
@@ -1,18 +1,30 @@
-// in test/e2e/main.spec.js
-describe('E2E: Utterence filter', function () {
+// tests for Utterence filter 
+describe('utterence filter', function () {
 
 	var ptor;
-	
-	browser.get('http://127.0.0.1:9000/');
 
-	beforeEach(function () {
-		ptor = protractor.getInstance();
+		browser.get('http://127.0.0.1:9000/');
 		// load demo
 		element(by.id('openDemoDBbtn')).click();
+	
+	// beforeEach it
+	beforeEach(function () {
+		ptor = protractor.getInstance();
 		element(by.input('filterText')).clear();
 	});
 
-	it('should find only 1 utt searching for 003', function () {
+	// afterEach it
+	afterEach(function () {
+		element(by.input('filterText')).clear();
+	});
+
+	it('should find only 0 utts searching for asdf', function () {
+		element(by.input('filterText')).sendKeys('asdf');
+		var elems = element.all(by.repeater('utt in uttList | regex:filterText'));
+		expect(elems.count()).toBe(0);
+	});
+
+	it('should find only 1 utts searching for 003', function () {
 		element(by.input('filterText')).sendKeys('003');
 		var elems = element.all(by.repeater('utt in uttList | regex:filterText'));
 		expect(elems.count()).toBe(1);
