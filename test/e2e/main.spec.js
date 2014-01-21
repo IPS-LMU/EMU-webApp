583d53436395882987a3bbb04353a3f78c816279
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2014-01-21 18:24:58 +0100

    added about e2e test

--- test/e2e/main.spec.js
+++ test/e2e/main.spec.js
@@ -2,16 +2,21 @@
 describe('E2E: main page', function () {
 
 	var ptor;
-	
 	browser.get('http://127.0.0.1:9000/');
 
-	beforeEach(function () {
-		ptor = protractor.getInstance();
-	});
+	describe('E2E: test about modal', function () {
+		// body...
+		beforeEach(function () {
+			ptor = protractor.getInstance();
+		});
 
-	it('should load the home page', function () {
-		var ele = by.input('filterText'); // see if filtertext input filed exists
-		expect(ptor.isElementPresent(ele)).toBe(true);
-	});
-
+		it('should load about modal', function () {
+			element(by.id('aboutBtn')).click();
+			heading = ptor.findElement(protractor.By.id('modalHeading'));
+			// console.log('#########################')
+			// console.log(heading.getText())
+			expect(heading.getText()).toEqual('EMU-webApp');
+			element(by.id('modalCancelBtn')).click('EMU-webApp');
+		});
+	})
 });
