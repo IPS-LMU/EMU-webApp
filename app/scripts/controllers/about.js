b1d192d6074857287d44db189647b23070370169
raphael winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2014-01-22 13:36:44 +0100

    more about fixes

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -5,8 +5,21 @@ angular.module('emulvcApp')
 		$scope.cps = ConfigProviderService;
 
 		$scope.getStrRep = function (code) {
-			
-			return String.fromCharCode(code);
+			var str;
+			switch (code) {
+			case 9:
+				str = 'TAB';
+				break;
+			case 16:
+				str = 'SHIFT';
+				break;
+			case 18:
+				str = 'ALT';
+				break;
+			default:
+				str = String.fromCharCode(code);
+			}
+			return str;
 		};
 
 		$scope.cancel = function () {
