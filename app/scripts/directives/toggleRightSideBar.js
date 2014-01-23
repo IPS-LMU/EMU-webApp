f16952ca3eefcf9e4b037a21d48e3cd75d4c3fdb
Raphael Winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2014-01-23 13:06:02 +0100

    more stuff for nganimate

--- app/scripts/directives/toggleRightSideBar.js
+++ app/scripts/directives/toggleRightSideBar.js
@@ -1,36 +1,5 @@
 'use strict';
 
-// angular.module('emulvcApp')
-// 	.directive('hideMe', function ($animate) {
-// 		return {
-// 			restrict: 'A',
-// 			link: function postLink(scope, element, attrs) {
-// 				scope.$watch(attrs.hideMe, function (newVal) {
-// 					if (newVal) {
-// 						$animate.addClass(element, 'fade')
-// 					} else {
-// 						$animate.removeClass(element, 'fade')
-// 					}
-// 				})
-// 			}
-// 		};
-// 	});
-
-
-// angular.module('emulvcApp').animation(".fade", function () {
-// 	return {
-// 		addClass: function (element, className) {
-// 			TweenMax.to(element, 1, {
-// 				opacity: 0
-// 			});
-// 		},
-// 		removeClass: function (element, className) {
-// 			TweenMax.to(element, 1, {
-// 				opacity: 1
-// 			});
-// 		}
-// 	}
-// });
 
 angular.module('emulvcApp')
 	.directive('showMenu', function ($animate) {
@@ -39,10 +8,9 @@ angular.module('emulvcApp')
 			link: function postLink(scope, element, attrs) {
 				scope.$watch(attrs.showMenu, function (newVal) {
 					if (newVal) {
-						alert('sdfsdf');
 						$animate.addClass(element, '.slideInMenu');
 					} else {
-						$animate.removeClass(element, 'slideInMenu');
+						$animate.removeClass(element, '.slideInMenu');
 					}
 				})
 			}
@@ -53,14 +21,17 @@ angular.module('emulvcApp')
 angular.module('emulvcApp').animation(".slideInMenu", function () {
 	return {
 		addClass: function (element, className) {
-			TweenMax.to(element, 1, {
-				opacity: 0
-			});
+			element.addClass('slideLeftTestClass');
+			// console.log(element)
+			// TweenMax.to(element, 1, {
+			// 	opacity: 0
+			// });
 		},
 		removeClass: function (element, className) {
-			TweenMax.to(element, 1, {
-				opacity: 1
-			});
+			// TweenMax.to(element, 1, {
+			// 	opacity: 1
+			// });
+			elements.removeClass('slideLeftTestClass');
 		}
 	}
 });
