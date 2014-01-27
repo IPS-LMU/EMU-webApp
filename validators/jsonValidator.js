9962de3a6f9194879652108c07c16b6ba60fa016
Raphael Winkelmann <raphaelwinkelmann@raphaels-MacBook-Pro.local>
2014-01-27 20:29:58 +0100

    validating up to constraints

--- validators/jsonValidator.js
+++ validators/jsonValidator.js
@@ -6,18 +6,27 @@ var os = require('os');
 var Validator = require('jsonschema').Validator;
 var v = new Validator();
 
-fs.readFile('schema.json', 'utf8', function (schemaErr, schemaData) {
-	if (!schemaErr) {
-		fs.readFile('globalDBconfig.json', 'utf8', function (err, instanceData) {
-			console.log(schemaData);
-			console.log(instanceData);
 
-			console.log(v.validate(JSON.parse(instanceData), JSON.parse(instanceData)));
 
+fs.readFile(process.argv[2], 'utf8', function (instanceErr, instanceData) {
+
+	fs.readFile('schemas/levelDefinitionSchema.json', 'utf8', function (levErr, levData) {
+		var curJson = JSON.parse(levData);
+		v.addSchema(curJson, curJson.id);
+
+		fs.readFile('schemas/attributeDefinitionSchema.json', 'utf8', function (attErr, attData) {
+			var curJson = JSON.parse(attData);
+			v.addSchema(curJson, curJson.id);
+
+			fs.readFile('schemas/constaintDefinitionSchema.json', 'utf8', function (conErr, conData) {
+				var curJson = JSON.parse(conData);
+				v.addSchema(curJson, curJson.id);
+
+				fs.readFile('schemas/globalDBschema.json', 'utf8', function (globErr, globData) {
+					console.log(v.validate(JSON.parse(instanceData), JSON.parse(globData)).errors);
+				});
+			});
 		});
-	} else {
-		console.log('Error reading schema');
-		console.log(schemaErr);
-	}
+	});
 
 });
