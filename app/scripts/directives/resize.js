88af351dc1753160d750ac2bbb8f266296254a92
georgraess <georg@raess.name>
2014-03-16 16:39:10 +0100

    improved resize directive

--- app/scripts/directives/resize.js
+++ app/scripts/directives/resize.js
@@ -7,49 +7,25 @@ angular.module('emuwebApp')
       link: function (scope, element) {
 
         var elem = element.parent().parent();
-        var canvas = elem.find('canvas');
-        var del = element.parent().children()[0];
-        var sav = element.parent().children()[2];
-        var bDelete = elem.find(del);
-        var bSave = elem.find(sav);
+        var canvas = element.parent().parent().find('canvas');
+        var deleteButton = elem.find(element.parent().children()[0]);
+        var resizeButton = elem.find(element.parent().children()[1]);
+        var saveButton = elem.find(element.parent().children()[2]);
         var open = true;
-        var cssLevel = 'Level';
-        var cssSmallLevel = 'smallLevel';
-        var cssLevelCanvas = 'LevelCanvas';
-        var cssLevelMarkupCanvas = 'LevelMarkupCanvas';
-        var cssSmallCanvas = 'smallCanvas';
 
         element.bind('click', function () {
           if (open) {
             open = false;
-            var small = {'height': '32px'};
-            elem.css(small)
-
-            // elem.parent().parent()[0].className = cssSmallLevel + ' ng-scope';
-            // canvas[0].className = cssLevelCanvas + ' ' + cssSmallCanvas;
-            // canvas[1].className = cssLevelMarkupCanvas + ' ' + cssSmallCanvas;
-            // if (scope.config.vals.activeButtons.deleteSingleLevel) {
-            //   bDelete.hide();
-            // }
-            // if (scope.config.vals.activeButtons.saveSingleLevel) {
-            //   bSave.hide();
-            // }
+            canvas.css({'height': '32px'});
+            resizeButton.css({'margin-top': '12px'});
+            deleteButton.hide();
+            saveButton.hide();
           } else {
             open = true;
-            var big = {'height': '64px'};
-            elem.css(big)
-            // console.log(elem);
-
-
-            // elem.parent().parent()[0].className = cssLevel + ' ng-scope';
-            // canvas[0].className = cssLevelCanvas;
-            // canvas[1].className = cssLevelMarkupCanvas;
-            // if (scope.config.vals.activeButtons.deleteSingleLevel) {
-            //   bDelete.show();
-            // }
-            // if (scope.config.vals.activeButtons.saveSingleLevel) {
-            //   bSave.show();
-            // }
+            canvas.css({'height': '64px'});
+            resizeButton.css({'margin-top': '0px'});
+            deleteButton.show();
+            saveButton.show();
           }
           scope.updateView();
 
