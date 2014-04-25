dc369e2eb7d50e24c4bab4b937fee478fbf451e0
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-04-25 14:31:23 +0200

    now have emuwebapp element in index.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,13 +3,13 @@
 
 angular.module('emuwebApp', ['ui', 'ui.bootstrap', 'ngRoute', 'ngAnimate'])
   .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/main.html'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
     $locationProvider.html5Mode(true);
 
   });
