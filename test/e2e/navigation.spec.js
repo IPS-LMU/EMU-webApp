236d620619d1ea7e068196b0c344e7cf372123dd
Georg Raess <raess@cip.ifi.lmu.de>
2014-06-25 11:57:59 +0200

    more e2e tests
    navigation bar / shortcuts part1

--- test/e2e/navigation.spec.js
+++ test/e2e/navigation.spec.js
@@ -15,7 +15,6 @@ describe('navigation', function () {
 	afterEach(function () {
 		element(by.id('zoomAllBtn')).click();
 	});	
-	
 
 	it('should have 2 bundles', function() {
 	    var elems = element.all(by.repeater('bundle in bundleList | regex:filterText'));
@@ -26,6 +25,114 @@ describe('navigation', function () {
 	    element(by.id('submenuOpen')).click();
 	});	
 	
+	it('should open & close submenu with shortcuts', function() {
+	    ptor.actions().sendKeys('o').perform();
+	    ptor.sleep(500);
+	    ptor.actions().sendKeys('o').perform();
+	    ptor.sleep(500);
+	});		
+	
+	it('should move around with zoom (with shortcuts)', function () {
+		for (var i = 0; i < 5; i++) {
+			ptor.actions().sendKeys('w').perform();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			ptor.actions().sendKeys('a').perform();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			ptor.actions().sendKeys('d').perform();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			ptor.actions().sendKeys('s').perform();
+		}
+	});	
+	
+	it('should move around with zoom (with navigationbar)', function () {
+		for (var i = 0; i < 3; i++) {
+			ptor.actions().sendKeys('w').perform();
+		}
+		var elem = element.all(by.css('.emuwebapp-previewMarkupCanvas')).get(0);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()		    
+		    .mouseDown()
+		    .mouseMove({ x: -100, y:0 })
+		    .mouseUp()
+		.perform();
+	    ptor.sleep(250);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()		    
+		    .mouseDown()
+		    .mouseMove({ x: 200, y:0 })
+		    .mouseUp()
+		.perform();
+	    ptor.sleep(250);	    
+	});
+	
+	it('should move around with zoom', function () {
+		for (var i = 0; i < 5; i++) {
+			element(by.id('zoomInBtn')).click();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			element(by.id('zoomLeftBtn')).click();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			element(by.id('zoomRightBtn')).click();
+		}
+
+		for (var i = 0; i < 5; i++) {
+			element(by.id('zoomOutBtn')).click();
+		}
+	});		
+
+	it('should overzoom to check boundaries for in and out', function () {
+		for (var i = 0; i < 30; i++) {
+			element(by.id('zoomInBtn')).click();
+		};
+
+		for (var i = 0; i < 30; i++) {
+			element(by.id('zoomOutBtn')).click();
+		};
+	});
+
+	it('should overzoom to check boundaries for left and right', function () {
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomInBtn')).click();
+		};
+
+		for (var i = 0; i < 30; i++) {
+			element(by.id('zoomLeftBtn')).click();
+		};
+		
+		for (var i = 0; i < 40; i++) {
+			element(by.id('zoomRightBtn')).click();
+		};
+	});
+	
+		
+	it('should tab in both directions', function() {
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomInBtn')).click();
+		};
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomRightBtn')).click();
+		};		
+	    ptor.actions().mouseMove(element(by.id('Phonetic'))).mouseMove( { x: -200, y: 0 }).click().perform();
+	    for (var i = 0; i < 4; i++) {
+	        ptor.actions().sendKeys(protractor.Key.TAB).perform();
+	        ptor.sleep(200);
+		};		    
+	    for (var i = 0; i < 4; i++) {
+	        ptor.actions().keyDown(protractor.Key.SHIFT).sendKeys(protractor.Key.TAB).keyUp(protractor.Key.SHIFT).perform();
+	        ptor.sleep(200);
+		};		    
+	});		
 
 	it('should move a boundary on SEGMENT tier', function() {
 		for (var i = 0; i < 3; i++) {
@@ -40,7 +147,9 @@ describe('navigation', function () {
 		    .mouseMove({ x: -80, y:0 })
 		    .keyUp(protractor.Key.SHIFT)
 		.perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
+	    ptor.actions().sendKeys('z').perform();	
+	    ptor.sleep(250);
 	});	
 	
 	it('should move a segment on SEGMENT tier', function() {
@@ -69,7 +178,7 @@ describe('navigation', function () {
 		    .mouseMove({ x: -100, y:0 })
 		    .keyUp(protractor.Key.SHIFT)
 		.perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
 	});			
 	
 	it('should open, rename and save on SEGMENT', function() {
@@ -86,7 +195,7 @@ describe('navigation', function () {
 	    expect(ptor.isElementPresent(area)).toBe(true);
 	    element(by.css('.emuwebapp-labelEdit')).sendKeys('TEST');
 	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
 	});
 	
 	it('should insert a new boundary on SEGMENT tier', function() {
@@ -101,7 +210,7 @@ describe('navigation', function () {
 		    .click()
 	    .perform();
 	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
 	});	
 	
 	it('should insert a new segment on SEGMENT tier', function() {
@@ -118,7 +227,7 @@ describe('navigation', function () {
 	       .mouseUp()
 	    .perform();
 	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
 	});	
 	
 	it('should insert a new element on EVENT tier', function() {
@@ -138,7 +247,7 @@ describe('navigation', function () {
 		    .click()
 	    .perform();
 	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
+	    ptor.sleep(250);
 	});	
 			
 	it('should open, rename and save on EVENT', function() {
@@ -156,8 +265,20 @@ describe('navigation', function () {
 	    expect(ptor.isElementPresent(area)).toBe(true);
 	    element(by.css('.emuwebapp-labelEdit')).sendKeys('testElem');
 	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
-	});		
+	    ptor.sleep(250);
+	});	
+	
+	it('should undo last 5 changes', function() {
+		var elem = element.all(by.css('.emuwebapp')).get(0);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()
+	    .perform();
+		for (var i = 0; i < 5; i++) {
+			ptor.actions().sendKeys('z').perform();	
+			ptor.sleep(250);
+		};					
+	});			
 	
 	it('should select a range in the viewport', function() {
 	   var elem = element.all(by.css('.emuwebapp-timelineCanvasMarkup')).get(0);
@@ -171,6 +292,10 @@ describe('navigation', function () {
 	   ptor.sleep(800);
 	});	
 	
+	it('should zoom in to the selected viewing range', function() {
+	   element(by.id('zoomSelBtn')).click();
+	});			
+	
 	it('should play sound in selected viewport', function() {
 	   var elem = element.all(by.css('.emuwebapp-timelineCanvasMarkup')).get(0);
 	   ptor.actions().mouseMove(elem).mouseMove({ x: -250, y: 0 }).mouseDown().mouseMove({ x: 250, y: 0 }).mouseUp().perform();
@@ -183,94 +308,16 @@ describe('navigation', function () {
 	   element(by.id('zoomInBtn')).click();	
 	   element(by.id('zoomLeftBtn')).click();	
 	   element(by.id('playViewBtn')).click();
-	   ptor.sleep(1000);
+	   ptor.sleep(900);
 	});		
 
 	it('should play complete sound', function () {
 		element(by.id('playAllBtn')).click();
 		ptor.sleep(2950);
-	});			
-	
-	it('should tab', function() {
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomInBtn')).click();
-		};
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomRightBtn')).click();
-		};		
-	    ptor.actions().mouseMove(element(by.id('Phonetic'))).mouseMove( { x: -200, y: 0 }).click().perform();
-	    ptor.actions().sendKeys(protractor.Key.TAB).perform();
-	    ptor.sleep(200);
-	    ptor.actions().sendKeys(protractor.Key.TAB).perform();
-	    ptor.sleep(200);
-	    ptor.actions().sendKeys(protractor.Key.TAB).perform();
-	    ptor.sleep(200);
-	    ptor.actions().sendKeys(protractor.Key.TAB).perform();	    
-	    ptor.sleep(200);
-	    ptor.actions().keyDown(protractor.Key.SHIFT).sendKeys(protractor.Key.TAB).keyUp(protractor.Key.SHIFT).perform();
-	    ptor.sleep(200);
-	    ptor.actions().keyDown(protractor.Key.SHIFT).sendKeys(protractor.Key.TAB).keyUp(protractor.Key.SHIFT).perform();
-	    ptor.sleep(200);
-	    ptor.actions().keyDown(protractor.Key.SHIFT).sendKeys(protractor.Key.TAB).keyUp(protractor.Key.SHIFT).perform();
-	    ptor.sleep(200);
-	    ptor.actions().keyDown(protractor.Key.SHIFT).sendKeys(protractor.Key.TAB).keyUp(protractor.Key.SHIFT).perform();
-	});	
-						
-	
-
-	it('should zoom in to the selected viewing range', function() {
-	   element(by.id('zoomSelBtn')).click();
-	});		
-
-	it('should zoom out to default view', function() {
-	   element(by.id('zoomAllBtn')).click();
 	});	
 
-	it('should overzoom to check boundaries for in and out', function () {
-		for (var i = 0; i < 30; i++) {
-			element(by.id('zoomInBtn')).click();
-		};
-
-		for (var i = 0; i < 30; i++) {
-			element(by.id('zoomOutBtn')).click();
-		};
-	});
-
-	it('should overzoom to check boundaries for left and right', function () {
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomInBtn')).click();
-		};
-
-		for (var i = 0; i < 30; i++) {
-			element(by.id('zoomLeftBtn')).click();
-		};
-		
-		for (var i = 0; i < 40; i++) {
-			element(by.id('zoomRightBtn')).click();
-		};
-
-		element(by.id('zoomAllBtn')).click();
-	});
 
 
-
-	it('should move around with zoom', function () {
-		for (var i = 0; i < 5; i++) {
-			element(by.id('zoomInBtn')).click();
-		}
-
-		for (var i = 0; i < 5; i++) {
-			element(by.id('zoomLeftBtn')).click();
-		}
-
-		for (var i = 0; i < 5; i++) {
-			element(by.id('zoomRightBtn')).click();
-		}
-
-		for (var i = 0; i < 5; i++) {
-			element(by.id('zoomOutBtn')).click();
-		}
-	});
 
 	
 	
