48985219239727ffbba32581ac21ab6e34a39c0f
Georg Raess <raess@cip.ifi.lmu.de>
2014-06-25 11:14:21 +0200

    e2e test now moves seg & seg boundary & element

--- test/e2e/navigation.spec.js
+++ test/e2e/navigation.spec.js
@@ -26,6 +26,69 @@ describe('navigation', function () {
 	    element(by.id('submenuOpen')).click();
 	});	
 	
+
+	it('should move a boundary on SEGMENT tier', function() {
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomInBtn')).click();
+			element(by.id('zoomRightBtn')).click();
+		};	
+		var elem = element.all(by.css('.emuwebapp-levelMarkupCanvas')).get(0);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()		    
+		    .keyDown(protractor.Key.SHIFT)
+		    .mouseMove({ x: -80, y:0 })
+		    .keyUp(protractor.Key.SHIFT)
+		.perform();
+	    ptor.sleep(1000);
+	});	
+	
+	it('should move a segment on SEGMENT tier', function() {
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomInBtn')).click();
+			element(by.id('zoomRightBtn')).click();
+		};	
+		var elem = element.all(by.css('.emuwebapp-levelMarkupCanvas')).get(0);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()
+		    .keyDown(protractor.Key.ALT)
+		    .mouseMove({ x: -100, y:0 })
+		    .keyUp(protractor.Key.ALT)
+		.perform();
+	    ptor.sleep(500);
+	});		
+	
+	it('should move a element on EVENT tier', function() {
+		var elem = element.all(by.css('.emuwebapp-levelMarkupCanvas')).get(1);
+		ptor.actions()
+		    .mouseMove(elem)
+		    .click()	
+		    .mouseMove({ x: -30, y:0 })	    
+		    .keyDown(protractor.Key.SHIFT)
+		    .mouseMove({ x: -100, y:0 })
+		    .keyUp(protractor.Key.SHIFT)
+		.perform();
+	    ptor.sleep(1000);
+	});			
+	
+	it('should open, rename and save a label', function() {
+		for (var i = 0; i < 3; i++) {
+			element(by.id('zoomInBtn')).click();
+			element(by.id('zoomRightBtn')).click();
+		};
+		var elem = element.all(by.css('.emuwebapp-levelMarkupCanvas')).get(0);
+		ptor.actions()
+		    .mouseMove(elem)
+	        .doubleClick()
+	    .perform();
+	    var area = by.css('.emuwebapp-labelEdit'); 
+	    expect(ptor.isElementPresent(area)).toBe(true);
+	    element(by.css('.emuwebapp-labelEdit')).sendKeys('TEST');
+	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
+	    ptor.sleep(1000);
+	});
+	
 	it('should select a range in the viewport', function() {
 	   var elem = element.all(by.css('.emuwebapp-timelineCanvasMarkup')).get(0);
 	   ptor.actions().mouseMove(elem).mouseMove({ x: -250, y: 0 }).mouseDown().mouseMove({ x: 250, y: 0 }).mouseUp().perform();
@@ -50,23 +113,7 @@ describe('navigation', function () {
 	it('should play complete sound', function () {
 		element(by.id('playAllBtn')).click();
 		ptor.sleep(2950);
-	});		
-	
-	it('should open, rename and save a label', function() {
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomInBtn')).click();
-		};
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomRightBtn')).click();
-		};		
-	    ptor.sleep(800);
-	    ptor.actions().mouseMove(element(by.id('Phonetic'))).mouseMove( { x: -200, y: 0 }).doubleClick().perform();
-	    var area = by.css('.emuwebapp-labelEdit'); 
-	    expect(ptor.isElementPresent(area)).toBe(true);
-	    element(by.css('.emuwebapp-labelEdit')).sendKeys('TEST');
-	    ptor.actions().sendKeys(protractor.Key.ENTER).perform();
-	    ptor.sleep(1000);
-	});	
+	});			
 	
 	it('should tab', function() {
 		for (var i = 0; i < 3; i++) {
@@ -94,17 +141,7 @@ describe('navigation', function () {
 	});	
 						
 	
-	it('should move a boundary', function() {
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomInBtn')).click();
-		};
-		for (var i = 0; i < 3; i++) {
-			element(by.id('zoomRightBtn')).click();
-		};		
-	    ptor.sleep(1000);
-	    ptor.actions().mouseMove(element(by.id('Phonetic'))).mouseMove( { x: -200, y: 0 }).mouseDown().mouseMove( { x: -200, y: 0 }).mouseUp().perform();
-	    ptor.sleep(1000);
-	});	
+
 	it('should zoom in to the selected viewing range', function() {
 	   element(by.id('zoomSelBtn')).click();
 	});		
