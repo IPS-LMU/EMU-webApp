c7d1ad705f3cb66a727cfc3ac32aa2d487643e78
Markus Jochim <markusjochim@phonetik.uni-muenchen.de>
2014-07-05 18:19:39 +0200

    First version that can draw a bunde using the d3 tree lib There is a lot of non-angulary code that needs cleaning up

--- app/scripts/controllers/ShowHierarchy.js
+++ app/scripts/controllers/ShowHierarchy.js
@@ -2,19 +2,22 @@
 
 angular.module('emuwebApp')
 	.controller('ShowhierarchyCtrl', function ($scope, dialogService, ConfigProviderService, Levelservice, HierarchyService) {
+		// Scope data
 		$scope.possiblePaths = [];
-
-		$scope.levels = ConfigProviderService.curDbConfig.levelDefinitions;
 
 		$scope.cancel = function () {
 			dialogService.close();
 		};
 
 		// Find non-ITEM levels to start calculating possible paths through the level hierarchy
-		
-		angular.forEach ($scope.levels, function (l) {
+		angular.forEach (ConfigProviderService.curDbConfig.levelDefinitions, function (l) {
 			if (l.type !== 'ITEM') {
 				$scope.possiblePaths = $scope.possiblePaths.concat(HierarchyService.findPaths(l.name));
 			}
 		});
+
+		// This must be angularised but I have yet to find out how
+		HierarchyService.setPath($scope.possiblePaths[0]);
+		setTimeout( HierarchyService.drawHierarchy, 1000);
+
 	});
