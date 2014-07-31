ce631786657dad17fba58fef98d17f6ac432d2a3
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-07-31 16:07:02 +0200

    build with new features
    incl. empty level fixes+preemph+heatmap

--- dist/scripts/workers/spectroWorker.js
+++ dist/scripts/workers/spectroWorker.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * A handy class to draw a spectrom (calculate a fft)
  *
@@ -11,7 +12,7 @@ var executed = false;
 var PI = 3.141592653589793; // value : Math.PI
 var TWO_PI = 6.283185307179586; // value : 2 * Math.PI
 var OCTAVE_FACTOR = 3.321928094887363; // value : 1.0/log10(2)	
-var emphasisPerOctave = 3.9810717055349722; // value : toLinearLevel(6);	
+var emphasisPerOctave; // = 3.9810717055349722; // value : toLinearLevel(6);	
 var sin, cos, n; // vars to hold sin and cos table	
 var internalalpha = 0.16;
 var totalMax = 0;
@@ -34,6 +35,20 @@ var myWindow = {
 	TRIANGULAR: 10
 };
 
+// worker scope global config var definitions
+var N;
+var upperFreq;
+var lowerFreq;
+var start;
+var end;
+var myStep;
+var myheight;
+var wFunction;
+var sampleRate;
+var streamChannels;
+var threadSoundBuffer;
+var drawHeatMapColors;
+var preEmphasisFilterFactor = 0.97;
 
 function FFT(fftSize) {
 	var m, alpha, func, i;
@@ -66,72 +81,102 @@ function FFT(fftSize) {
 		}
 	}
 
-	/*
-    // choose window function set alpha and execute it on the buffer 
-    //
-	// [parameters]
-	//
-	// type			-> chosen Window Function
-	// alpha		-> alpha for Window Functions (default 0.16)
-	// buffer		-> current fft window data
-	//
-	// [return]
-	//	
-	// calculated FFT Window
-	*/
+	/**
+	 * choose window function set alpha and execute it on the buffer
+	 *
+	 * [parameters]
+	 *
+	 * type			-> chosen Window Function
+	 * alpha		-> alpha for Window Functions (default 0.16)
+	 * buffer		-> current fft window data
+	 *
+	 * [return]
+	 *
+	 * calculated FFT Window
+	 */
 	this.wFunction = function (type, alpha, buffer) {
 		var length = buffer.length;
 		this.alpha = alpha;
 		switch (type) {
 		case myWindow.BARTLETT:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionBartlett(length, i);
 			}
 			break;
 		case myWindow.BARTLETTHANN:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionBartlettHann(length, i);
 			}
 			break;
 		case myWindow.BLACKMAN:
 			this.alpha = this.alpha || 0.16;
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionBlackman(length, i, alpha);
 			}
 			break;
 		case myWindow.COSINE:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionCosine(length, i);
 			}
 			break;
 		case myWindow.GAUSS:
 			this.alpha = this.alpha || 0.25;
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionGauss(length, i, alpha);
 			}
 			break;
 		case myWindow.HAMMING:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionHamming(length, i);
 			}
 			break;
 		case myWindow.HANN:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionHann(length, i);
 			}
 			break;
 		case myWindow.LANCZOS:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionLanczos(length, i);
 			}
 			break;
 		case myWindow.RECTANGULAR:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionRectangular(length, i);
 			}
 			break;
 		case myWindow.TRIANGULAR:
 			for (i = 0; i < length; i++) {
+				if (i > 0) {
+					buffer[i] = this.applyPreEmph(buffer[i], buffer[i - 1]);
+				}
 				buffer[i] *= this.wFunctionTriangular(length, i);
 			}
 			break;
@@ -183,6 +228,12 @@ function FFT(fftSize) {
 
 	this.wFunctionTriangular = function (length, index) {
 		return 2 / length * (length / 2 - Math.abs(index - (length - 1) / 2));
+	};
+	/**
+	 * calculate and apply according pre-emphasis on sample
+	 */
+	this.applyPreEmph = function (curSample, prevSample) {
+		return curSample - preEmphasisFilterFactor * prevSample;
 	};
 
 	// the FFT calculation
@@ -239,31 +290,34 @@ function FFT(fftSize) {
 
 
 
-/* 
-	// initial function call for calculating and drawing Spectrogram
-	// input PCM data is coming from the buffer "localSoundBuffer"
-	// which has to be filled before.
-	// - first loop calculates magnitudes to draw (getMagnitude())
-	// - second loop draws values on canvas  (drawOfflineSpectogram())
-	//
-	// [parameters]
-	//
-	// N			-> window Size
-	// upperFreq	-> upper boundry in Hz
-	// start		-> start time
-	// end			-> end time
-	// cWidth		-> width of canvas element
-	// cHeight		-> height of canvas element
-	// octx			-> Context of Canvas Element used for drawing 
-	*/
+/**
+ * initial function call for calculating and drawing Spectrogram
+ * input sample data comes from the buffer "localSoundBuffer"
+ * which has to be filled before.
+ * - first loop calculates magnitudes to draw (getMagnitude())
+ * - second loop draws values on canvas  (drawOfflineSpectogram())
+ *
+ * [parameters]
+ *
+ * N			-> window Size
+ * upperFreq	-> upper boundry in Hz
+ * start		-> start time
+ * end			-> end time
+ * cWidth		-> width of canvas element
+ * cHeight		-> height of canvas element
+ * octx			-> Context of Canvas Element used for drawing
+ */
 
-var parseData = (function (N, upperFreq, lowerFreq, start, end, renderWidth, renderHeight, pixelRatio, transparency) {
-	return function (N, upperFreq, lowerFreq, start, end, renderWidth, renderHeight, pixelRatio, transparency) {
+var parseData = (function (N, upperFreq, lowerFreq, start, end, renderWidth, renderHeight, pixelRatio, transparency, drawHeatMapColors, preEmphasisPerOctaveInDb) {
+	return function (N, upperFreq, lowerFreq, start, end, renderWidth, renderHeight, pixelRatio, transparency, drawHeatMapColors, preEmphasisPerOctaveInDb) {
 
 		if (!executed) {
 
 			// start execution once
 			executed = true;
+
+			// calculate emphasisPerOctave
+			emphasisPerOctave = toLinearLevel(preEmphasisPerOctaveInDb);
 
 			// instance of FFT with windowSize N
 			myFFT = new FFT(N);
@@ -280,22 +334,22 @@ var parseData = (function (N, upperFreq, lowerFreq, start, end, renderWidth, ren
 			// lower Hz boundary to display
 			d = Math.floor(lowerFreq / HzStep); // -1 for value below display when lower>0
 
+			// height between two interpolation points
+			pixelHeight = renderHeight / (c - d - 2);
+
+			// create new picture
+			var imageResult = new Uint8ClampedArray(Math.ceil(renderWidth * renderHeight * 4));
+
 			// calculate i FFT runs, save result into paint and set maxPsd while doing so
 			for (var i = 0; i < renderWidth; i++) {
 				paint[i] = getMagnitude(0, Math.round(i * myStep) + myOffset, N, c, d);
 				maxPsd = (2 * Math.pow(totalMax, 2)) / N;
 			}
 
-			// height between two interpolation points
-			pixelHeight = renderHeight / (c - d - 2);
-
-			// create new picture
-			imageResult = new Uint8ClampedArray(Math.ceil(renderWidth * renderHeight * 4));
-
 			// draw spectrogram on png image with canvas width
 			// (one column is drawn in drawOfflineSpectogram)
 			for (var j = 0; j < renderWidth; j++) {
-				drawOfflineSpectogram(j, imageResult, c, d, myDrawOffset, renderWidth, renderHeight, transparency);
+				drawOfflineSpectogram(j, imageResult, c, d, myDrawOffset, renderWidth, renderHeight, transparency, drawHeatMapColors);
 			}
 
 			// post generated image block with settings back
@@ -320,34 +374,34 @@ var parseData = (function (N, upperFreq, lowerFreq, start, end, renderWidth, ren
 	};
 })();
 
-/*
-	// calculates Magnitude by 
-	// - reading the current (defined with offset) data from localSoundBuffer
-	// - applying the current Window Function to the selected data
-	// - calculating the actual FFT
-	// - (and saving the biggest value in totalMax)
-	//
-	// [parameters]
-	//
-	// channel			-> Number of Channels
-	// offset			-> Calculated offset in PCM Stream
-	// windowSize		-> Size of Window used for calculation
-	// c				-> Upper Boundry (c = Math.floor(upperFreq/HzStep);)
-	//
-	// [return]
-	//
-	// calculated FFT data as Float32Array
-	*/
+/**
+ * calculates Magnitude by
+ * - reading the current (defined with offset) data from localSoundBuffer
+ * - applying the current Window Function to the selected data
+ * - calculating the actual FFT
+ * - (and saving the biggest value in totalMax)
+ *
+ * [parameters]
+ *
+ * channel			-> Number of Channels
+ * offset			-> Calculated offset in PCM Stream
+ * windowSize		-> Size of Window used for calculation
+ * c				-> Upper Boundry (c = Math.floor(upperFreq/HzStep);)
+ 
+ * [return]
+ 
+ * calculated FFT data as Float32Array
+ */
 
 function getMagnitude(channel, offset, windowSize, c, d) {
 	// imaginary array of length N
-	imag = new Float32Array(N);
+	var imag = new Float32Array(N);
 
 	// real array of length N
-	real = new Float32Array(N);
+	var real = new Float32Array(N);
 
 	// result array of length N
-	result = new Float32Array(c - d);
+	var result = new Float32Array(c - d);
 
 	// set real values by reading local sound buffer
 	for (var j = 0; j < windowSize; j++) {
@@ -359,8 +413,10 @@ function getMagnitude(channel, offset, windowSize, c, d) {
 
 	// calculate FFT over real and save to result
 	myFFT.fft(real, imag);
+	// calculate magnitude for each spectral component 
 	for (var low = 0; low <= c - d; low++) {
 		result[low] = magnitude(real[low + d], imag[low + d]);
+		// result[low] = result[low] * low * emphasisPerOctave; // preemphasis per octave SIC SIC SIC... don't use
 		if (totalMax < result[low]) {
 			totalMax = result[low];
 		}
@@ -368,27 +424,53 @@ function getMagnitude(channel, offset, windowSize, c, d) {
 	return result;
 }
 
-/*
-	// draws a single Line on the Canvas Element
-	// by calculating the RGB value of the current pixel with:
-	// 255-(255*scaled)
-	// function has to be called in an outer loop (according to canvas_width)
-	// the inner loop draws a single line on the canvas (according to canvas_height)
-	//
-	// [parameters]
-	//
-	// line			-> calculated FFT Data
-	//
-	*/
+/**
+ * interpolates a 3D color space and calculate accoring
+ * value on that plane
+ *
+ * @param minval is the minimum value to map to (number)
+ * @param maxval is the maximum value to map to (number)
+ * @param val is the value itself (number)
+ * @param colors is an array of arrays containing the colors
+ * to interpol. against (of the form: [[255, 0, 0],[0, 255, 0],[0, 0, 255]])
+ */
+function convertToHeatmap(minval, maxval, val, colors) {
+	var maxIndex = colors.length - 1;
+	var v = (val - minval) / (maxval - minval) * maxIndex;
+	var i1 = Math.floor(v);
+	var i2 = Math.min.apply(null, [Math.floor(v) + 1, maxIndex]);
+	var rgb1 = colors[i1];
+	var rgb2 = colors[i2];
+	var f = v - i1;
+	return ({
+		'r': Math.floor(rgb1[0] + f * (rgb2[0] - rgb1[0])),
+		'g': Math.floor(rgb1[1] + f * (rgb2[1] - rgb1[1])),
+		'b': Math.floor(rgb1[2] + f * (rgb2[2] - rgb1[2]))
+	});
+
+}
+
+/**
+ * draws a single Line on the Canvas Element
+ * by calculating the RGB value of the current pixel with:
+ * 255-(255*scaled)
+ * function has to be called in an outer loop (according to canvas_width)
+ * the inner loop draws a single line on the canvas (according to canvas_height)
+ *
+ * [parameters]
+ *
+ * line			-> calculated FFT Data
+ *
+ */
 
 function drawOfflineSpectogram(line, p, c, d, cacheOffet, renderWidth, renderHeight, transparency) {
 
 	// set upper boundary for linear interpolation
 	var x1 = pixelHeight;
 	// value for first interpolation at lower boundry (height=0)
-	psd = (2 * Math.pow(paint[line][1], 2)) / N;
-	psdLog = 10 * log10(psd / maxPsd);
-	scaledVal = ((psdLog + dynRangeInDB) / dynRangeInDB);
+	var psd = (2 * Math.pow(paint[line][1], 2)) / N;
+	var psdLog = 10 * log10(psd / maxPsd);
+	var scaledVal = ((psdLog + dynRangeInDB) / dynRangeInDB);
 	if (scaledVal > 1) {
 		scaledVal = 1;
 	} else if (scaledVal < 0) {
@@ -396,7 +478,7 @@ function drawOfflineSpectogram(line, p, c, d, cacheOffet, renderWidth, renderHei
 	}
 
 
-	for (var i = 1; i < paint[line].length; i++) {
+	for (var i = 0; i < paint[line].length; i++) {
 
 		var y0 = scaledVal; // !!!! set y0 to previous scaled value
 
@@ -425,20 +507,41 @@ function drawOfflineSpectogram(line, p, c, d, cacheOffet, renderWidth, renderHei
 		if (pixelHeight >= 1) {
 			// do interpolation between y0 (previous scaledValue) and y1 (scaledValue now)
 			for (var b = 0; b < pixelHeight; b++) {
-				y2 = y0 + (y1 - y0) / x1 * b;
+				var y2 = y0 + (y1 - y0) / x1 * b;
 
 				// calculate corresponding color value for interpolation point [0...255]
-				rgb = 255 - Math.round(255 * y2);
+				var rgb = 255 - Math.round(255 * y2);
 
 				// set internal image buffer to calculated & interpolated value
 				var px = Math.floor(line + cacheOffet);
 				var py = Math.floor(myheight - (pixelHeight * (i - 2) + b));
 
 				var index = (px + (py * renderWidth)) * 4;
-				p[index + 0] = rgb;
-				p[index + 1] = rgb;
-				p[index + 2] = rgb;
-				p[index + 3] = transparency;
+				if (drawHeatMapColors) {
+					if (!isNaN(rgb)) { // SIC!!! Why do we have NaNs as rgb vals?
+						var hmVals = convertToHeatmap(0, 255, rgb, [
+							[255, 0, 0],
+							[0, 255, 0],
+							[0, 0, 0]
+						]);
+						p[index + 0] = hmVals.r;
+						p[index + 1] = hmVals.g;
+						p[index + 2] = hmVals.b;
+						p[index + 3] = transparency;
+
+					} else {
+						p[index + 0] = rgb;
+						p[index + 1] = rgb;
+						p[index + 2] = rgb;
+						p[index + 3] = transparency;
+					}
+
+				} else {
+					p[index + 0] = rgb;
+					p[index + 1] = rgb;
+					p[index + 2] = rgb;
+					p[index + 3] = transparency;
+				}
 			}
 		} else {
 			rgb = 255 - Math.round(255 * y1);
@@ -462,22 +565,22 @@ function drawOfflineSpectogram(line, p, c, d, cacheOffet, renderWidth, renderHei
  */
 
 // decimal color [0...255] to hex color
-function d2h(d) {
-	return (+d).toString(16);
-}
+// function d2h(d) {
+// 	return (+d).toString(16);
+// }
 
 // used by FFT
-function toLevelInDB(linearLevel) {
-	if (linearLevel < 0) {
-		alert('Linear level argument must be positive.');
-	}
-	return 10 * log10(linearLevel);
-}
+// function toLevelInDB(linearLevel) {
+// 	if (linearLevel < 0) {
+// 		alert('Linear level argument must be positive.');
+// 	}
+// 	return 10 * log10(linearLevel);
+// }
 
 // used by FFT
-function getLevelInDB(linearLevel) {
-	return toLevelInDB(linearLevel);
-}
+// function getLevelInDB(linearLevel) {
+// 	return toLevelInDB(linearLevel);
+// }
 
 // used by FFT
 function toLinearLevel(dbLevel) {
@@ -495,46 +598,46 @@ function magnitude(real, imag) {
 }
 
 // used to calculate current packet
-function getPacketInPercent(packets, percent) {
-	return Math.floor(packets / 100 * percent);
-}
+//function getPacketInPercent(packets, percent) {
+//	return Math.floor(packets / 100 * percent);
+//}
 
-/*
-    // Web Worker Communication
-    //
-    // Steps:
-    // ------
-    //
-    // (1)	Setup Web Worker by calling "config" and corresponding parameter
-    //		N		--> window Size
-    //		freq	--> upper Frequency Boundry
-    //		start	--> start Value in
-    //		end		--> end Value in 
-    //		window	-->	window Function (please use myWindow enum)
-    //		width	--> height of canvas used to display
-    //		height	--> width of canvas used to display
-    //
-    //		- example: primeWorker.postMessage({'cmd': 'config', 'N': N});
-    //
-    //
-    // (2)	Send PCM Configuration Data (ie header of pcm data) to Web Worker by calling "pcm","config"
-    //
-    //		- example:	var data = JSON.stringify(sourceBuffer);
-    //					primeWorker.postMessage({'cmd': 'pcm', 'config': data});		
-    //
-    //
-    // (3)	Send PCM Data by calling "pcm","stream"
-    //
-    //		- example:	primeWorker.postMessage({'cmd': 'pcm', 'stream': sourceBuffer.getChannelData(0)});		
-    //
-    //
-    // (4)	Start complete calculation by calling "render"
-    //
-    //		- example	primeWorker.postMessage({'cmd': 'render'});
-    //
-    //
-    //	--> Wait for callback of Web Worker sending you Base64 encoded spectrogram image
-    */
+/**
+ * Web Worker Communication
+ *
+ * Steps:
+ * ------
+ *
+ * (1)	Setup Web Worker by calling "config" and corresponding parameter
+ *		N		--> window Size
+ *		freq	--> upper Frequency Boundry
+ *		start	--> start Value in
+ *		end		--> end Value in
+ *		window	-->	window Function (please use myWindow enum)
+ *		width	--> height of canvas used to display
+ *		height	--> width of canvas used to display
+ *
+ *		- example: primeWorker.postMessage({'cmd': 'config', 'N': N});
+ *
+ *
+ * (2)	Send PCM Configuration Data (ie header of pcm data) to Web Worker by calling "pcm","config"
+ *
+ *		- example:	var data = JSON.stringify(sourceBuffer);
+ *					primeWorker.postMessage({'cmd': 'pcm', 'config': data});
+ *
+ *
+ * (3)	Send PCM Data by calling "pcm","stream"
+ *
+ *		- example:	primeWorker.postMessage({'cmd': 'pcm', 'stream': sourceBuffer.getChannelData(0)});
+ *
+ *
+ * (4)	Start complete calculation by calling "render"
+ *
+ *		- example	primeWorker.postMessage({'cmd': 'render'});
+ *
+ *
+ *	--> Wait for callback of Web Worker sending you Base64 encoded spectrogram image
+ */
 
 self.addEventListener('message', function (e) {
 	var data = e.data;
@@ -619,9 +722,16 @@ self.addEventListener('message', function (e) {
 		if (data.stream !== undefined) {
 			threadSoundBuffer = new Float32Array(data.stream);
 		}
+		if (data.drawHeatMapColors !== undefined) {
+			drawHeatMapColors = data.drawHeatMapColors;
+		}
+		if (data.preEmphasisFilterFactor !== undefined) {
+			preEmphasisFilterFactor = data.preEmphasisFilterFactor;
+		}
 		break;
 	case 'render':
-		parseData(N, upperFreq, lowerFreq, start, end, mywidth, myheight, pixelRatio, transparency);
+		console.log(preEmphasisFilterFactor);
+		parseData(N, upperFreq, lowerFreq, start, end, mywidth, myheight, pixelRatio, transparency, drawHeatMapColors, preEmphasisFilterFactor);
 		break;
 	default:
 		self.postMessage('Unknown command: ' + data.msg);
