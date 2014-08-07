1578d04679b75d9d33c13c936bf8a2b70c2cc0e9
Georg Raess <raess@cip.ifi.lmu.de>
2014-08-07 15:52:21 +0200

    added Firefox to unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function (config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS','Chrome'],
+    browsers: ['PhantomJS','Chrome','Firefox'],
 
 
     // Continuous Integration mode
