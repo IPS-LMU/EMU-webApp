ac31c7c94c235bd9bb9883b4842f881b6aaba77e
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-08-27 16:28:37 +0200

    removed log

--- app/scripts/controllers/SelectThresholdModalCtrl.js
+++ app/scripts/controllers/SelectThresholdModalCtrl.js
@@ -5,7 +5,6 @@ angular.module('emuwebApp')
 		$scope.passedInOpts = passedInOpts;
 
 		$scope.myData = ArrayHelperService.convertArrayToXYjsoArray(passedInOpts.y);
-		console.log($scope.myData);
 
 		$scope.select = function (idx) {
 			dialogService.close(idx);
