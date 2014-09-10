629b0409358599221b56c649d490d4e795df0877
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-09-10 14:03:37 +0200

    messing with unit tests

--- test/spec/services/Espsparserservice.spec.js
+++ test/spec/services/Espsparserservice.spec.js
@@ -5,39 +5,39 @@ describe('Service: Espsparserservice', function () {
 
   // load the controller's module
   beforeEach(module('emuwebApp'));
-  
-  beforeEach(inject(function(_$rootScope_, $q, Espsparserservice) {
+
+  beforeEach(inject(function (_$rootScope_, $q, Espsparserservice) {
     scope = _$rootScope_;
-    deferred = $q.defer();
-    deferred.resolve('called'); //  always resolved, you can do it from your spec
-    spyOn(Espsparserservice, 'asyncParseJSO').andReturn(deferred.promise); 
-    spyOn(Espsparserservice, 'asyncParseEsps').andReturn(deferred.promise); 
+    // deferred = $q.defer();
+    // deferred.resolve('called'); //  always resolved, you can do it from your spec
+    // spyOn(Espsparserservice, 'asyncParseJSO').andReturn(deferred.promise);
+    // spyOn(Espsparserservice, 'asyncParseEsps').andReturn(deferred.promise);
   }));
-  
- /**
+
+  /**
    *
    */
   // it('should resolve asyncParseJSO', inject(function (Espsparserservice, LevelService) {
-     // var result;
-     // LevelService.setData(mockaeMsajc003);
-     // //spyOn(Espsparserservice, 'asyncParseJSO').and.returnValue(deferred.promise);
-     // var ret = Espsparserservice.asyncParseJSO('Utterance').then(function (res) {
-     //  expect(res).toEqual('called');
-     // });
-     // scope.$apply();
+  //   var result;
+  //   LevelService.setData(msajc003_bndl.annotation);
+  //   //spyOn(Espsparserservice, 'asyncParseJSO').and.returnValue(deferred.promise);
+  //   Espsparserservice.asyncParseJSO('Utterance').then(function (res) {
+  //     expect(res).toEqual('called');
+  //   });
+  //   scope.$apply();
   // }));
-   
- /**
+
+  /**
    *
    */
   // it('should resolve asyncParseEsps', inject(function (Espsparserservice, LevelService) {
-     // var result;
-     // LevelService.setData(mockaeMsajc003);
-     // //spyOn(Espsparserservice, 'asyncParseJSO').and.returnValue(deferred.promise);
-     // var ret = Espsparserservice.asyncParseEsps('','','').then(function (res) {
-     //  expect(res).toEqual('called');
-     // });
-     // scope.$apply();
+  // var result;
+  // LevelService.setData(mockaeMsajc003);
+  // //spyOn(Espsparserservice, 'asyncParseJSO').and.returnValue(deferred.promise);
+  // var ret = Espsparserservice.asyncParseEsps('','','').then(function (res) {
+  //  expect(res).toEqual('called');
+  // });
+  // scope.$apply();
   // }));
-  
+
 });
