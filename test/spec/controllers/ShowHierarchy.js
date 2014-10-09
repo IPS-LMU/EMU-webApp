1dac83acb2481ace9a96359ef33ac203796b2738
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-10-09 15:46:50 +0200

    bad test commented out

--- test/spec/controllers/ShowHierarchy.js
+++ test/spec/controllers/ShowHierarchy.js
@@ -9,14 +9,14 @@ describe('Controller: ShowhierarchyCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ShowhierarchyCtrl = $controller('ShowhierarchyCtrl', {
-      $scope: scope
-    });
-  }));
+  // beforeEach(inject(function ($controller, $rootScope) {
+  //   scope = $rootScope.$new();
+  //   ShowhierarchyCtrl = $controller('ShowhierarchyCtrl', {
+  //     $scope: scope
+  //   });
+  // }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
 });
