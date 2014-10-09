10d1aeacaf9e19dee38ad017e767fee8e28f7483
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-10-09 11:51:47 +0200

    default only phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -117,7 +117,7 @@ module.exports = function (config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome','PhantomJS'],
+    browsers: ['PhantomJS'],
     
     captureTimeout: 60000,
 
