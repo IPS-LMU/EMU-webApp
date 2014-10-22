649594de566400585a48daef5336c371d392d7e1
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-10-22 18:05:23 +0200

    removed scope.this statements

--- app/scripts/directives/save.js
+++ app/scripts/directives/save.js
@@ -5,7 +5,7 @@ angular.module('emuwebApp')
     return {
       restrict: 'A',
       link: function (scope, element, attr) {
-        var name = scope.this.level.name;
+        var name = scope.level.name;
 
         element.bind('click', function () {
           scope.vs.setcurClickLevelName(name, attr.save);
