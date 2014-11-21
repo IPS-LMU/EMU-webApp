c0e613f006662e0b6878581396ed9cdd6315dcbb
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2014-11-21 17:19:48 +0100

    fixed e2e tests

--- test/e2e/spectroSettings.spec.js
+++ test/e2e/spectroSettings.spec.js
@@ -1,25 +1,25 @@
-function selectOption(selector, item){
-    var selectList, desiredOption;
+function selectOption(selector, item) {
+	var selectList, desiredOption;
 
-    selectList = this.findElement(selector);
-    selectList.click();
+	selectList = this.findElement(selector);
+	selectList.click();
 
-    selectList.findElements(protractor.By.tagName('option'))
-        .then(function findMatchingOption(options){
-            options.some(function(option){
-                option.getText().then(function doesOptionMatch(text){
-                    if (item === text){
-                        desiredOption = option;
-                        return true;
-                    }
-                });
-            });
-        })
-        .then(function clickOption(){
-            if (desiredOption){
-                desiredOption.click();
-            }
-        });
+	selectList.findElements(protractor.By.tagName('option'))
+		.then(function findMatchingOption(options) {
+			options.some(function (option) {
+				option.getText().then(function doesOptionMatch(text) {
+					if (item === text) {
+						desiredOption = option;
+						return true;
+					}
+				});
+			});
+		})
+		.then(function clickOption() {
+			if (desiredOption) {
+				desiredOption.click();
+			}
+		});
 }
 
 // tests for spectro Settings
@@ -33,153 +33,140 @@ describe('E2E: spectrogram settings', function () {
 	});
 
 	// afterEach it
-	afterEach(function () {
-	});
+	afterEach(function () {});
 
-	it('should be able to set window Func to BARLETT and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	/////////////////////////////////
+	// start: test different window functions
+
+	it('should be able to set window Func to BARLETT', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'BARLETT');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
-	
-	it('should be able to set window Func to BARLETTHANN and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	});
+
+	it('should be able to set window Func to BARLETTHANN', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'BARLETTHANN');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
+	});
 
-	it('should be able to set window Func to BLACKMAN and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to BLACKMAN', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'BLACKMAN');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
+	});
 
-	it('should be able to set window Func to COSINE and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to COSINE', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'COSINE');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
+	});
 
-	it('should be able to set window Func to HAMMING and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to HAMMING', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'HAMMING');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
+	});
 
-	it('should be able to set window Func to HANN and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to HANN', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'HANN');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	 
-	
-	it('should be able to set window Func to LANCZOS and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	});
+
+	it('should be able to set window Func to LANCZOS', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'LANCZOS');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});	
+	});
 
-	it('should be able to set window Func to RECTANGULAR and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to RECTANGULAR', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'RECTANGULAR');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
 	});
 
-	it('should be able to set window Func to TRIANGULAR and window Len to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
+	it('should be able to set window Func to TRIANGULAR', function () {
+		element(by.id('spectSettingsBtn')).click();
 		ptor.selectOption = selectOption.bind(ptor);
 		ptor.selectOption(by.id('selWindowInfo'), 'TRIANGULAR');
-		ptor.selectOption(by.id('windowLength'), '1024');
 		element(by.id('dialogSaveButton')).click();
 		ptor.sleep(90);
-	});		
+	});
 
-	it('should be able to set window Length to 32', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '32');
+	// end: test different window functions
+	///////////////////////////////
+
+	/////////////////////////////////
+	// start: test different window sizes
+
+	it('should be able to set window size to 0.001', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.model('modalVals.windowSizeInSecs')).clear();
+		element(by.model('modalVals.windowSizeInSecs')).sendKeys('0.001');
 		element(by.id('dialogSaveButton')).click();
 	});
-	
-	it('should be able to set window Length to 32', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '32');
-		element(by.id('dialogSaveButton')).click();
-	});	
 
-	it('should be able to set window Length to 128', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '128');
+	it('should be able to set window size to 0.005', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.model('modalVals.windowSizeInSecs')).clear();
+		element(by.model('modalVals.windowSizeInSecs')).sendKeys('0.005');
 		element(by.id('dialogSaveButton')).click();
-	});	
-	
-	it('should be able to set window Length to 512', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '512');
-		element(by.id('dialogSaveButton')).click();
-	});	
-	
-	it('should be able to set window Length to 1024', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '1024');
-		element(by.id('dialogSaveButton')).click();
-	});	
-	
-	it('should be able to set window Length to 2048', function () {
-		element(by.id('spectSettingsBtn')).click();	
-		ptor.selectOption = selectOption.bind(ptor);
-		ptor.selectOption(by.id('windowLength'), '2048');
-		element(by.id('dialogSaveButton')).click();
-	});	
+	});
 
+	it('should be able to set window size to 0.1', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.model('modalVals.windowSizeInSecs')).clear();
+		element(by.model('modalVals.windowSizeInSecs')).sendKeys('0.1');
+		element(by.id('dialogSaveButton')).click();
+	});
+
+	// end: test different window sizes
+	//////////////////////////////////////
+
+	//////////////////////////////////////
+	// start: test rangeTo/rangeFrom
 	it('should be able to set view range of spectrogram settings top limit to 8000 Hz', function () {
 		element(by.id('spectSettingsBtn')).click();
-		element(by.model('modalVals.rangeTo')).clear()
+		element(by.model('modalVals.rangeTo')).clear();
 		element(by.model('modalVals.rangeTo')).sendKeys('8000');
-		element(by.id('dialogSaveButton')).click();
-		var ele = by.model('filterText'); 
-		expect(ptor.isElementPresent(ele)).toBe(true);
-	});
-
-	it('should be able to set view range of spectrogram settings bottom limit to 2000 Hz', function () {
-		element(by.id('spectSettingsBtn')).click();
-		element(by.model('modalVals.rangeFrom')).clear()
-		element(by.model('modalVals.rangeFrom')).sendKeys('1000');
 		element(by.id('dialogSaveButton')).click();
 		var ele = by.model('filterText');
 		expect(ptor.isElementPresent(ele)).toBe(true);
 	});
 
+	it('should be able to set view range of spectrogram settings bottom limit to 2000 Hz', function () {
+		element(by.id('spectSettingsBtn')).click();
+		element(by.model('modalVals.rangeFrom')).clear();
+		element(by.model('modalVals.rangeFrom')).sendKeys('1000');
+		element(by.id('dialogSaveButton')).click();
+		var ele = by.model('filterText');
+		expect(ptor.isElementPresent(ele)).toBe(true);
+	});
+	// end: test rangeTo/rangeFrom
+	/////////////////////////////////////
+
+	/////////////////////////////////////
+	// start: test dynamic range
 	it('should be able to set dynamic range to 50', function () {
 		element(by.id('spectSettingsBtn')).click();
-		element(by.model('modalVals.dynamicRange')).clear()
+		element(by.model('modalVals.dynamicRange')).clear();
 		element(by.model('modalVals.dynamicRange')).sendKeys('50');
 		element(by.id('dialogSaveButton')).click();
 		var ele = by.model('filterText');
@@ -188,11 +175,14 @@ describe('E2E: spectrogram settings', function () {
 
 	it('should be able to set dynamic range to 90', function () {
 		element(by.id('spectSettingsBtn')).click();
-		element(by.model('modalVals.dynamicRange')).clear()
+		element(by.model('modalVals.dynamicRange')).clear();
 		element(by.model('modalVals.dynamicRange')).sendKeys('90');
 		element(by.id('dialogSaveButton')).click();
 		var ele = by.model('filterText');
 		expect(ptor.isElementPresent(ele)).toBe(true);
 	});
+
+	// end: test dynamic range
+	/////////////////////////////////////
 
 });
