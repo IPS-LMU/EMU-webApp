a2ca5b1b9934f99be33de680d5f9ba5fdd455932
Georg Raess <georg.raess@campus.lmu.de>
2015-02-19 13:49:41 +0100

    more unit tests

--- test/spec/services/HierarchyLayoutService.spec.js
+++ test/spec/services/HierarchyLayoutService.spec.js
@@ -2,26 +2,50 @@
 
 describe('Service: HierarchyLayoutService', function () {
 
-   // load the service's module
-   beforeEach(module('emuWebAppApp'));
+  var scope;
 
-  // // instantiate service
-   var HierarchyLayoutService;
-   beforeEach(inject(function (_HierarchyLayoutService_) {
-     HierarchyLayoutService = _HierarchyLayoutService_;
-   }));
+   // load the service's module
+   beforeEach(module('emuwebApp'));
+
+  // instantiate service
+    beforeEach(inject(function ($rootScope, 
+                                LevelService,
+                                modalService,
+                                viewState,
+                                DataService,
+                                ConfigProviderService,
+                                HierarchyLayoutService) {
+        // scopes
+        scope = $rootScope.$new();
+        scope.data = DataService;
+        scope.cps = ConfigProviderService;
+        scope.cps.setVals(defaultEmuwebappConfig);
+        scope.cps.curDbConfig = aeDbConfig;
+        scope.data.setData(msajc003_bndl.annotation);
+        scope.hierarchy = HierarchyLayoutService;
+        scope.lvl = LevelService;
+        scope.modal = modalService;
+        scope.vs = viewState;
+    }));
+
 
   /**
    *
-
-  it('should findParents', function () {
-    spyOn(LevelService, 'getLevelDetails').and.returnValue({ level: { items: []} });
-    scope.modal.open('template','dataIn','dataExport');
-    expect(scope.modal.dataIn).toEqual('dataIn');
-    expect(scope.modal.dataExport).toEqual('dataExport');
-    expect(scope.modal.templateUrl).toEqual('template');
-    expect(scope.vs.setState).toHaveBeenCalled();    
-  });
    */  
+  it('should findParents', function () {
+    spyOn(scope.hierarchy, 'findChildren').and.returnValue([{},{},{}]);
+    spyOn(scope.lvl, 'getLevelDetails').and.returnValue({ level: { items: [1, 2, 3]} });
+    scope.hierarchy.findParents('Phonetic');
+    expect(scope.hierarchy.findChildren).toHaveBeenCalled();
+  });
+  
+  /**
+   *
+   */  
+  it('should findChildren', function () {
+    spyOn(scope.lvl, 'getLevelDetails').and.returnValue({ level: { items: [1, 2, 3]} });
+    var children = scope.hierarchy.findChildren({id:147}, 'Phonetic');
+    expect(children).toEqual([]);
+  });
 
 });
