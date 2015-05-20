f7226451343f61f3381e1c7d243098744143d7e4
Georg <georg@georg.bramm.vpn>
2015-05-20 13:20:45 +0200

    small changes

--- app/scripts/directives/drawssff.js
+++ app/scripts/directives/drawssff.js
@@ -227,7 +227,7 @@ angular.module('emuwebApp')
                     var rightVal = rightBorder[contourNr];
 
                     curSampleInCol = colEndSampleNr + 1;
-                    curSampleInColTime = (1 / sR * curSampleInCol) + sT;
+                    curSampleInColTime = (1 / sR * curSampleInCol) + sT ;
 
                     x = (curSampleInColTime - startTimeVP) / (endTimeVP - startTimeVP) * canvas.width;
                     y = canvas.height - ((rightVal - minVal) / (maxVal - minVal) * canvas.height);
@@ -246,13 +246,13 @@ angular.module('emuwebApp')
               var horizontalText;
               
               if (nrOfSamples <= 2) {
-                horizontalText = fontScaleService.getTextImageTwoLines(ctx, 'Zoom out to', 'see contour(s)', ConfigProviderService.design.font.small.size.slice(0,-2), ConfigProviderService.design.font.large.family, ConfigProviderService.design.color.red);
+                horizontalText = fontScaleService.getTextImageTwoLines(ctx, 'Zoom out to', 'see contour(s)', ConfigProviderService.design.font.small.size.slice(0,-2) / 1.05 , ConfigProviderService.design.font.small.family, ConfigProviderService.design.color.transparent.red);
                 // ctx.fillStyle = ConfigProviderService.vals.colors.levelColor;
                 // ctx.fillRect(0, 0, canvas.width, canvas.height);
-                ctx.drawImage(horizontalText, 0, 0, horizontalText.width, horizontalText.height, canvas.width / 2 - horizontalText.width / 2, 25, horizontalText.width, horizontalText.height);
+                ctx.drawImage(horizontalText, 0, 0, horizontalText.width, horizontalText.height, canvas.width/2 - horizontalText.width / 2, 25, horizontalText.width, horizontalText.height);
               } else {
                 txt = 'Zoom in to see contour(s)';
-                horizontalText = fontScaleService.getTextImageTwoLines(ctx, 'Zoom in to', 'see contour(s)', ConfigProviderService.design.font.small.size.slice(0,-2), ConfigProviderService.design.font.large.family, ConfigProviderService.design.color.transparent.red);
+                horizontalText = fontScaleService.getTextImageTwoLines(ctx, 'Zoom in to', 'see contour(s)', ConfigProviderService.design.font.small.size.slice(0,-2) / 1.05, ConfigProviderService.design.font.small.family, ConfigProviderService.design.color.transparent.red);
                 // ctx.fillStyle = ConfigProviderService.vals.colors.levelColor;
                 // ctx.fillRect(0, 0, canvas.width, canvas.height);              
                 ctx.drawImage(horizontalText, 0, 0, horizontalText.width, horizontalText.height, canvas.width / 2 - horizontalText.width / 2, 25, horizontalText.width, horizontalText.height);
