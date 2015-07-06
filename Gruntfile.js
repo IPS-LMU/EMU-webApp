5cb1b6e7bea978005d8fdb9f0a211547c673eed2
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-07-06 14:21:39 +0200

    deeper copy for help tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -302,7 +302,7 @@ module.exports = function (grunt) {
         files: [{
           expand: true,
           cwd: '<%= yeoman.app %>',
-          src: ['*.html', 'views/*.html'],
+          src: ['*.html', 'views/*.html', 'views/helpTabs/*.html'],
           dest: '<%= yeoman.dist %>'
         }]
       }
