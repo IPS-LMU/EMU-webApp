9138ff388b9b9109b586617a95edc3a3e96ff87c
Georg Raess <georg.raess@campus.lmu.de>
2015-07-21 12:56:17 +0200

    e2e: fixed uttFilter and added first hierarchy version

---
+++ test/e2e/hierarchy.spec.js
@@ -0,0 +1,17 @@
+// tests for Utterence filter
+describe('hierarchy', function () {
+
+	var ptor;
+
+	browser.get('http://127.0.0.1:9000/');
+	// beforeEach it
+	beforeEach(function () {
+		ptor = protractor.getInstance();
+	});
+
+	// afterEach it
+	afterEach(function () {
+	});
+
+
+});
--- test/e2e/uttFilter.spec.js
+++ test/e2e/uttFilter.spec.js
@@ -1,42 +1,45 @@
-// tests for Utterence filter 
+// tests for Utterence filter
 describe('utterence filter', function () {
 
 	var ptor;
 
 	browser.get('http://127.0.0.1:9000/');
 
-
 	// beforeEach it
 	beforeEach(function () {
 		ptor = protractor.getInstance();
-		element(by.model('filterText')).clear();
+
 	});
-	
 
 	// afterEach it
 	afterEach(function () {
 		element(by.model('filterText')).clear();
 	});
 
-/* disabled 
 	it('should find only 0 utts searching for asdf', function () {
-		element(by.model('filterText')).sendKeys('asdf');
-		var elems = element.all(by.repeater("(key, value) in lmds.getBundleList() | groupBy: 'session'"));
+		var menuOpen = element.all(by.id('submenuOpen'));
+		ptor.actions()
+			.mouseMove(menuOpen.get(0))
+			.click()
+			.perform();
+		ptor.sleep(500);
+		element(by.model('filterText')).sendKeys('asdfghj');
+		ptor.sleep(500);
+		var elems = element.all(by.id("uttListItem"));
 		expect(elems.count()).toBe(0);
 	});
 
 	it('should find only 1 utts searching for 003', function () {
-		element(by.model('filterText')).sendKeys('0');
-		var elems = element.all(by.repeater("(key, value) in lmds.getBundleList() | groupBy: 'session'"));
-		expect(elems.count()).toBe(2);
-	});
-
-	it('should find 3 utts searching for 01', function () {
-		element(by.model('filterText')).sendKeys('03');
-		var elems = element.all(by.repeater("(key, value) in lmds.getBundleList() | groupBy: 'session'"));
+		element(by.model('filterText')).sendKeys('003');
+		ptor.sleep(500);
+		var elems = element.all(by.id("uttListItem"));
 		expect(elems.count()).toBe(1);
 	});
-	
-	*/
 
+	it('should find 2 utts searching for 0', function () {
+		element(by.model('filterText')).sendKeys('0');
+		ptor.sleep(500);
+		var elems = element.all(by.id("uttListItem"));
+		expect(elems.count()).toBe(2);
+	});
 });
