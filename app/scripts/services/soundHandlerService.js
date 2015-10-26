3048977d1273926d0adcf42e5fad251faa0640b4
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2015-10-26 19:24:14 +0100

    use webaudio API as player to circumvent chrome base64 in src of audioelement audio bug

--- app/scripts/services/soundHandlerService.js
+++ app/scripts/services/soundHandlerService.js
@@ -2,20 +2,71 @@
 
 angular.module('emuwebApp')
 	.service('Soundhandlerservice', function Soundhandlerservice($document, Binarydatamaniphelper) {
+
+		// private vars
+		var audioContext;
+		var curSource;
+
 		// shared service object
 		var sServObj = {};
 
+		// public vars
 		sServObj.wavJSO = {};
+		sServObj.isPlaying = false;
 
-		sServObj.player = $document[0].createElement('audio');
-		sServObj.player.isPlaying = false;
+		///////////////////////////////////////////
+		// private API
 
-		sServObj.setPlayerSrc = function (buf) {
-			var base64String = Binarydatamaniphelper.arrayBufferToBase64(buf);
-			this.player.src = 'data:audio/wav;base64,' + base64String;
+		/**
+		 * safely initialize audio context
+		 * */
+		function initAudioContext(){
+			try {
+				// Fix up prefixing
+				window.AudioContext = window.AudioContext || window.webkitAudioContext;
+				audioContext = new AudioContext();
+			}catch(e){
+				alert("Error loading the AudioContext (could mean your browser doesn't support the HTML5 webaudio API):" + e);
+			}
+		}
+
+		///////////////////////////////////////////
+		// public API
+
+		/**
+		 * set audioBuffer of entire audio file
+		 *
+		 * @param buffer arraybuffer containing audio file (as returned by XHR for example)
+		 * */
+		sServObj.decodeAndPlay = function(buffer){
+			if(typeof(audioContext) == "undefined"){
+				initAudioContext();
+			}
+
+			audioContext.decodeAudioData(buffer, function(ab) {
+				curSource = audioContext.createBufferSource();
+				curSource.buffer = ab;
+				curSource.connect(audioContext.destination);
+				curSource.start(0);
+				curSource.onended = function(){
+					sServObj.isPlaying = false;
+				}
+
+			}, function(e){
+				alert(e);
+			});
 		};
 
-		sServObj.resetPlayerSrcFromTo = function (sampleStart, endSample) {
+
+		/**
+		 * Creates new ArrayBuffer containing wav file that goes
+		 * from sampleStart to endSample
+		 *
+		 * @param sampleStart number that represents start sample
+		 * @paramn endSample number that represents end sample
+		 * @returns ArrayBuffer containing wav file
+		 * */
+		sServObj.extractRelPartOfWav = function (sampleStart, endSample) {
 			var bytePerSample = sServObj.wavJSO.BitsPerSample / 8;
 			var header = sServObj.wavJSO.origArrBuf.subarray(0, 44);
 			var data = sServObj.wavJSO.origArrBuf.subarray(44, sServObj.wavJSO.Data.length * bytePerSample);
@@ -31,32 +82,29 @@ angular.module('emuwebApp')
 			tmp.set(new Uint8Array(header), 0);
 			tmp.set(new Uint8Array(newData), header.byteLength);
 
-			var base64String = Binarydatamaniphelper.arrayBufferToBase64(tmp.buffer);
-			sServObj.player.src = 'data:audio/wav;base64,' + base64String;
+			return(tmp.buffer);
 		};
 
+		/**
+		 * play audio from to specified in samples
+		 *
+		 * @param sampleStart number that represents start sample
+		 * @param endSample number that represents end sample
+		 * */
 		sServObj.playFromTo = function (sampleStart, endSample) {
-			if (!this.player.isPlaying) {
-				sServObj.player = $document[0].createElement('audio');
-				sServObj.player.onerror = function(e){
-					alert(e)
-				};
-			}
-			this.resetPlayerSrcFromTo(sampleStart, endSample);
 
-			if (this.player.isPlaying) {
-				this.player.isPlaying = false;
-				this.player.pause();
+			var cutWavBuff = this.extractRelPartOfWav(sampleStart, endSample);
+
+			if (sServObj.isPlaying) {
+				sServObj.isPlaying = false;
+				curSource.stop(0);
 			} else {
-				this.player.isPlaying = true;
-				this.player.play();
+
+				sServObj.isPlaying = true;
+				sServObj.decodeAndPlay(cutWavBuff);
 			}
 
 		};
-
-		sServObj.player.addEventListener('ended', function () {
-			this.isPlaying = false;
-		}, false);
 
 
 		return sServObj;
