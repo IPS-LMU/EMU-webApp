79a7a09be697e1032dc941a5cea6ef7bf1842b85
ru34toq <georg.raess@campus.lmu.de>
2015-11-13 13:55:59 +0100

    connect history

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('emuwebApp')
-	.controller('ModalCtrl', function ($scope, ArrayHelperService, modalService, viewState, LevelService, HistoryService, ConfigProviderService) {
+	.controller('ModalCtrl', function ($scope, ArrayHelperService, browserDetector, modalService, viewState, LevelService, HistoryService, ConfigProviderService) {
 
 		$scope.cps = ConfigProviderService;
 		$scope.data = undefined;
+		$scope.mySelect = null;
 
 		/**
 		 *
@@ -36,6 +37,54 @@ angular.module('emuwebApp')
 		$scope.discardChanges = function (name) {
 			modalService.close();
 		};
+		
+		/**
+		 *  Save a URL
+		 */
+		$scope.saveURL = function () {
+		    var currentURLS = $scope.getURLs();
+		    if(currentURLS.indexOf(modalService.dataOut) === -1 ) {
+    		    currentURLS.push(modalService.dataOut);		    
+		    }		 
+		    localStorage.setItem("urls", JSON.stringify(currentURLS));
+		    $scope.myUrls = currentURLS;
+		    $scope.mySelect = $scope.myUrls[0];
+		};		
+		
+		/**
+		 *  Return all URLs from localStorage
+		 */
+		$scope.getURLs = function () {
+		    var curVal = localStorage.getItem("urls");
+		    var urlData = [];
+		    if(!browserDetector.isBrowser.PhantomJS() && curVal !== null){
+		        urlData = JSON.parse(curVal);
+		    }
+		    return urlData;
+		};	
+		
+		/**
+		 *  Return all URLs from localStorage
+		 */
+		$scope.setCurrentURL = function (data) {
+		    modalService.dataOut = data;
+		};	
+		
+		
+		/**
+		 *  delete a specific url
+		 */
+		$scope.deleteURL = function (data) {
+		    var currentURLS = $scope.getURLs();
+		    if(currentURLS.indexOf(data) !== -1 ) {
+    		    currentURLS.splice(currentURLS.indexOf(data), 1);		    
+		    }		 
+		    localStorage.setItem("urls", JSON.stringify(currentURLS));
+		    $scope.myUrls = currentURLS;
+		    $scope.mySelect = $scope.myUrls[0];
+		};							
+
+
 
 
 		/**
--- app/scripts/directives/modal.js
+++ app/scripts/directives/modal.js
@@ -34,7 +34,6 @@ angular.module('emuwebApp')
 			    scope.force = modalService.force;
 			}
 		});
-
       }
     };
   });
