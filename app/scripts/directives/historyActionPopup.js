2aeb8286ac095d3e743da09ee6c912fdaa546a04
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-11-08 14:36:25 +0100

    newest angular version (1.5.8)

--- app/scripts/directives/historyActionPopup.js
+++ app/scripts/directives/historyActionPopup.js
@@ -13,13 +13,11 @@ angular.module('emuwebApp')
 				scope.$watch('vs.historyActionTxt', function () {
 					if (scope.vs.historyActionTxt !== '') {
 						$animate.addClass(element, 'emuwebapp-history-fade').then(function () {
-							scope.$apply();
 							$timeout(function () {
 								$animate.removeClass(element, 'emuwebapp-history-fade').then(function () {
 									$timeout(function () {
 										scope.vs.historyActionTxt = '';
 									}, scope.cps.design.animation.period);
-									scope.$apply();
 								})
 							}, scope.cps.design.animation.period);
 						});
