b74a7e247b0ee01e7a645ecbfb5382e05796e971
Raphael Winkelmann <raphael.winkelmann@gmx.de>
2016-11-14 12:19:47 +0100

    working through tests

--- test/spec/workers/ssffParserWorker.spec.js
+++ test/spec/workers/ssffParserWorker.spec.js
@@ -3,12 +3,12 @@
 describe('Worker: ssffParserWorker', function() {
 
   var worker, mockGlobal, wavData;
-    
+
   beforeEach(module('emuwebApp'));
-  
+
   beforeEach(function(){
     var DummyWorker = function() {};
-    worker = new ssffParserWorker(DummyWorker);    
+    worker = new ssffParserWorker(DummyWorker);
     // mock the global scope for the worker thread.
     mockGlobal = {
       postMessage: jasmine.createSpy('postMessage')
@@ -16,8 +16,8 @@ describe('Worker: ssffParserWorker', function() {
     // call the initWorker method we use to build the worker script.
     worker.workerInit(mockGlobal);
   });
-  
- 
+
+
   it('should respond properly to undefined msg', function() {
     mockGlobal.onmessage('unknown');
     expect(mockGlobal.postMessage).toHaveBeenCalledWith({
@@ -27,7 +27,7 @@ describe('Worker: ssffParserWorker', function() {
 		}
 	});
   });
-  
+
   it('should respond properly to defined msg with unknown cmd', function() {
     mockGlobal.onmessage({data: {cmd: 'unknown'}});
     expect(mockGlobal.postMessage).toHaveBeenCalledWith({
@@ -37,31 +37,31 @@ describe('Worker: ssffParserWorker', function() {
 		}
 	});
   });
-  
-  it('should base64encode', function () { 
+
+  it('should base64encode', function () {
     var ret = mockGlobal.btoa('test');
     expect(ret).toEqual('dGVzdA==');
     var ret = mockGlobal.atob('dGVzdA==');
-    expect(ret).toEqual('test');    
+    expect(ret).toEqual('test');
   });
-  
+
   it('should parseArr', function () {
     mockGlobal.onmessage({data: {
       'cmd': 'parseArr',
       'ssffArr': msajc003_bndl.ssffFiles
-    }});    
+    }});
     expect(mockGlobal.postMessage).toHaveBeenCalled();
       // todo : need small testable data
-  });  
-  
+  });
+
   it('should parseArr', function () {
     mockGlobal.onmessage({data: {
         'cmd': 'jso2ssff',
         'jso': JSON.stringify({ Columns: [ { name: 'test1', ssffdatatype: 'SHORT', length: 10, values: [[1]] } ], origFreq: 20 })
-      }});    
+      }});
       expect(mockGlobal.postMessage).toHaveBeenCalled();
       // todo : need small testable data
   });
-  
-  
+
+
 });
