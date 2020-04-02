295c93fbb5474ca6739d526697af05354d9006ec
Raphael Winkelmann <raphael@phonetik.uni-muenchen.de>
2020-04-02 12:04:27 +0200

    more d3 v3 -> v5 updates

--- app/scripts/services/app-cache-handler.service.ts
+++ app/scripts/services/app-cache-handler.service.ts
@@ -9,10 +9,10 @@ class AppcacheHandlerService{
 	constructor($http, ModalService){
 		this.$http = $http;
 		this.ModalService = ModalService;
-		this.appCache = window.applicationCache;
+		// this.appCache = window.applicationCache;
 
 		// // bind evts
-		if(typeof this.appCache !== 'undefined') {
+		// if(typeof this.appCache !== 'undefined') {
 			// appCache.addEventListener('progress', handleProgressEvent, false);
 
 			// appCache.addEventListener('checking', handleCheckingEvent, false);
@@ -20,28 +20,28 @@ class AppcacheHandlerService{
 			// appCache.addEventListener('downloading', handleDownloadingEvent, false);
 			// appCache.addEventListener('progress', handleProgressEvent, false);
 			// appCache.addEventListener('cached', handleCachedEvent, false);
-			this.appCache.addEventListener('updateready', this.handleUpdatereadyEvent, false);
+			// this.appCache.addEventListener('updateready', this.handleUpdatereadyEvent, false);
 
 			// appCache.addEventListener('obsolete', handleObsoleteEvent, false);
 			// appCache.addEventListener('error', handleErrorEvent, false);
-		}
+		// }
 	}
 
 		/**
 		 *
 		 */
 		private handleUpdatereadyEvent() {
-            if(typeof this.appCache !== 'undefined') {
-                this.ModalService.open('views/confirmModal.html', 'A new version of the EMU-WebApp is available and has already been downloaded and cached in your browser. Would you like to use it? CAUTION: A reload will delete all current changes... TIP: the next time you use the EMU-webApp you will automatically use the updated version)').then((res) => {
-                    if (res) {
-                        localStorage.removeItem('haveShownWelcomeModal');
-                        this.appCache.swapCache();
-                        window.location.reload();
-                    } else {
-                        localStorage.removeItem('haveShownWelcomeModal');
-                    }
-                });
-            }
+            // if(typeof this.appCache !== 'undefined') {
+            //     this.ModalService.open('views/confirmModal.html', 'A new version of the EMU-WebApp is available and has already been downloaded and cached in your browser. Would you like to use it? CAUTION: A reload will delete all current changes... TIP: the next time you use the EMU-webApp you will automatically use the updated version)').then((res) => {
+            //         if (res) {
+            //             localStorage.removeItem('haveShownWelcomeModal');
+            //             this.appCache.swapCache();
+            //             window.location.reload();
+            //         } else {
+            //             localStorage.removeItem('haveShownWelcomeModal');
+            //         }
+            //     });
+            // }
 		};
 
 
@@ -51,12 +51,12 @@ class AppcacheHandlerService{
 
 		public checkForNewVersion(){
 			// console.log('check for new version');
-            if(typeof this.appCache !== 'undefined') {
-				if ((this.appCache.status !== 0 && this.appCache.status !== 3)) { // uncached == 0 & downloading == 3
-					console.log('INFO: appCache.status: ' + this.appCache.status);
-					this.appCache.update();
-				}
-            }
+            // if(typeof this.appCache !== 'undefined') {
+			// 	if ((this.appCache.status !== 0 && this.appCache.status !== 3)) { // uncached == 0 & downloading == 3
+			// 		console.log('INFO: appCache.status: ' + this.appCache.status);
+			// 		this.appCache.update();
+			// 	}
+            // }
 
             if ('serviceWorker' in navigator) {
 				// console.log("service worker available")
